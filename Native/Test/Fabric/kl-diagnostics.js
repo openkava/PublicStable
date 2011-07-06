51a6851cbcd83727bd50d9bb75f0b41e27fbe61e
Peter Zion <peter.zion@gmail.com>
2011-07-06 17:41:47 -0400

    Rename kl-diagnostics unit test and verify that it doesn't leak

--- Native/Test/Fabric/kl-diagnostics.js
+++ Native/Test/Fabric/kl-diagnostics.js
@@ -1,5 +1,5 @@
-
-FABRIC = wrapFabricClient(createFabricClient());
+FC = createFabricClient();
+FABRIC = wrapFabricClient(FC);
 
 op = FABRIC.DependencyGraph.createOperator( "op" );
 print( op.getDiagnostics().length );
@@ -10,3 +10,6 @@ print( op.getDiagnostics()[0].desc );
 
 op.setSourceCode("operator entry( io Scalar input, io Scalar output ) { output = 2 * input; }");
 print( op.getDiagnostics().length );
+
+FABRIC.flush();
+FC.dispose();
