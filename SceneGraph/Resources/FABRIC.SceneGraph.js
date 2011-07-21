6320213381b155df7ad7b3ca2baecf373b66c787
Phil Taylor <phil.taylor@fabric-engine.com>
2011-07-21 09:20:57 -0400

    * Shortened Manipulator node names, and ensured they were consistent. * Fixed bug in eeror message generation in CheckVBORequirements. * If a shader has no material uniforms, then it doesn't construct a dgnode. * Shadow material is customizable on the scene constructor.

--- SceneGraph/Resources/FABRIC.SceneGraph.js
+++ SceneGraph/Resources/FABRIC.SceneGraph.js
@@ -441,7 +441,7 @@ FABRIC.SceneGraph = {
     };
     scene.getShadowMapMaterial = function() {
       if (!shadowMapMaterial) {
-        shadowMapMaterial = this.pub.constructNode('ShadowMaterial', {
+        shadowMapMaterial = this.pub.constructNode(sceneOptions.shadowMaterial, {
           parentEventHandler: beginRenderShadowMap
         });
       }
@@ -559,9 +559,9 @@ FABRIC.SceneGraph = {
 
     /////////////////////////////////////////////////////////////////////
     // Public Scene Interface
-    sceneOptions = sceneOptions ? sceneOptions : {};
-    scene.assignDefaults(sceneOptions, {
+    sceneOptions = scene.assignDefaults(sceneOptions, {
         constructGlobalsNode: true,
+        shadowMaterial:'ShadowMaterial',
         constructAnimationInterface: true,
         fixedTimeStep: true,
         timeStep: 20
