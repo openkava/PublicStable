5bf63fc3ae0e543142e1ab1f1f619242e335d0cc
Phil Taylor <phil.taylor@fabric-engine.com>
2011-07-21 21:02:04 -0400

    * By constructing the redraw event handlers with the name 'Redraw', the 'getRedrawEventHandler' interface function is automatically generated.  - Note: when 'constructEventHandlerNode('Redraw')' is called, it assigns/replaces the method 'getRedrawEventHandler'. * Operators can be constructed with empty bindings arrays.

--- SceneGraph/Resources/FABRIC.SceneGraph.Materials.js
+++ SceneGraph/Resources/FABRIC.SceneGraph.Materials.js
@@ -46,7 +46,7 @@ FABRIC.SceneGraph.registerNodeType('Image',
     }
 
     // Construct the handler for loading the image into texture memory.
-    redrawEventHandler = imageNode.constructEventHandlerNode('Draw');
+    redrawEventHandler = imageNode.constructEventHandlerNode('Redraw');
     redrawEventHandler.addScope('image', dgnode);
     redrawEventHandler.addMember('bufferID', 'Size', 0);
     if (options.createLoadTextureEventHandler) {
@@ -63,9 +63,6 @@ FABRIC.SceneGraph.registerNodeType('Image',
             ]
           }));
     }
-    imageNode.getRedrawEventHandler = function() {
-      return redrawEventHandler;
-    };
 
     imageNode.loadImageAsset = function(imageURL) {
       url = imageURL;
@@ -229,7 +226,7 @@ FABRIC.SceneGraph.registerNodeType('PointSpriteTexture',
     var dgnode = pointSpriteTextureNode.constructDGNode('DGNode');
     dgnode.addMember('resolution', 'Integer', options.spriteResolution);
 
-    var redrawEventHandler = pointSpriteTextureNode.constructEventHandlerNode('Draw');
+    var redrawEventHandler = pointSpriteTextureNode.constructEventHandlerNode('Redraw');
     redrawEventHandler.addScope('image', dgnode);
     redrawEventHandler.addMember('bufferID', 'Size', 0);
     redrawEventHandler.preDescendBindings.append(scene.constructOperator({
@@ -242,10 +239,6 @@ FABRIC.SceneGraph.registerNodeType('PointSpriteTexture',
               'textureStub.textureUnit'
             ]
           }));
-
-    pointSpriteTextureNode.getRedrawEventHandler = function() {
-      return redrawEventHandler;
-    };
 
     return pointSpriteTextureNode;
   });
@@ -332,6 +325,7 @@ FABRIC.shaderAttributeTable = {
   shininess: { id: 34, type: 'Scalar' },
   bumpiness: { id: 35, type: 'Scalar' },
   normalLength: { id: 36, type: 'Scalar' },
+  depthScale: { id: 37, type: 'Scalar' },
 
   light: { id: 50, type: 'Vec3' },
   lightType: { id: 51, type: 'Integer' },
@@ -383,7 +377,7 @@ FABRIC.SceneGraph.registerNodeType('Shader',
         parentEventHandler: scene.getSceneRedrawEventHandler()
       });
     var shaderNode = scene.constructNode('SceneGraphNode', options),
-      redrawEventHandler = shaderNode.constructEventHandlerNode('Draw'),
+      redrawEventHandler = shaderNode.constructEventHandlerNode('Redraw'),
       shaderSources = [],
       uniformValues = [],
       programParams = [],
@@ -459,6 +453,17 @@ FABRIC.SceneGraph.registerNodeType('Shader',
         'self.program'
       ]
     }));
+    
+    if (options.autoSetProgram) {
+      redrawEventHandler.preDescendBindings.append(scene.constructOperator({
+        operatorName: 'useProgramOp',
+        srcFile: 'FABRIC_ROOT/SceneGraph/Resources/KL/loadShader.kl',
+        entryFunctionName: 'useProgram',
+        parameterBinding: [
+          (options.separateShaderNode ? 'shader.program' : 'self.program')
+        ]
+      }));
+    }
 
     if (options.parentEventHandler !== false) {
       // The shader is registered with the scenegraph, which will
@@ -482,7 +487,8 @@ FABRIC.SceneGraph.registerNodeType('Material',
   function(options, scene) {
     scene.assignDefaults(options, {
         autoSetProgram: true,
-        separateShaderNode: true
+        separateShaderNode: true,
+        shaderNode: undefined
       });
 
     var materialNode,
@@ -491,9 +497,7 @@ FABRIC.SceneGraph.registerNodeType('Material',
       materialType = options.type,
       shaderPrefix,
       index,
-      shaderName,
       shader,
-      operators,
       addAccessors,
       uniform,
       uniformName,
@@ -515,34 +519,34 @@ FABRIC.SceneGraph.registerNodeType('Material',
       }
     }
     if (options.separateShaderNode) {
-      shaderName = materialType + 'Shader' + (options.shaderNameDecoration !== undefined ?
-                                              options.shaderNameDecoration : '');
-
-      shader = scene.constructShaderNode(shaderName, {
-        name: shaderName,
-        fragmentShader: options.fragmentShader,
-        vertexShader: options.vertexShader,
-        tessControlShader: options.tessControlShader,
-        tessEvalShader: options.tessEvalShader,
-        geometryShader: options.geometryShader,
-        shaderUniforms: options.shaderUniforms,
-        shaderAttributes: options.shaderAttributes,
-        programParams: options.programParams,
-        parentEventHandler: options.parentEventHandler
-      });
-
+      if(options.shaderNode){
+        shader = options.shaderNode;
+      }
+      else{
+        var shaderName = materialType + 'Shader' + (options.shaderNameDecoration !== undefined ?
+                                                options.shaderNameDecoration : '');
+  
+        shader = scene.constructShaderNode(shaderName, {
+          name: shaderName,
+          fragmentShader: options.fragmentShader,
+          vertexShader: options.vertexShader,
+          tessControlShader: options.tessControlShader,
+          tessEvalShader: options.tessEvalShader,
+          geometryShader: options.geometryShader,
+          shaderUniforms: options.shaderUniforms,
+          shaderAttributes: options.shaderAttributes,
+          programParams: options.programParams,
+          parentEventHandler: options.parentEventHandler,
+          autoSetProgram: options.autoSetProgram
+        });
+      }
 
       materialNode = scene.constructNode('SceneGraphNode', options);
-      dgnode = materialNode.constructDGNode('DGNode');
-      redrawEventHandler = materialNode.constructEventHandlerNode('Draw');
-
+      redrawEventHandler = materialNode.constructEventHandlerNode('Redraw');
       shader.getRedrawEventHandler().appendChildEventHandler(redrawEventHandler);
 
       materialNode.getShaderNode = function() {
         return shader;
-      };
-      materialNode.getRedrawEventHandler = function() {
-        return redrawEventHandler;
       };
       materialNode.getVBORequirements = function() {
         return shader.getVBORequirements();
@@ -552,31 +556,16 @@ FABRIC.SceneGraph.registerNodeType('Material',
       // The shader and the material properties are stored in the same node.
       // we simply extend the shader with the material paramters. 
       materialNode = scene.constructNode('Shader', options);
-      dgnode = materialNode.constructDGNode('DGNode');
       redrawEventHandler = materialNode.getRedrawEventHandler();
     }
+    
     if(constructDGNode){
-      dgnode = materialNode.getDGNode();
+      dgnode = materialNode.constructDGNode('DGNode');
       redrawEventHandler.addScope('material', dgnode);
     }
 
     /////////////////////////////////
     // Material Binding operator
-    scene.pushTimer('generatingMaterialOperators');
-
-    operators = redrawEventHandler.preDescendBindings;
-    if (options.autoSetProgram) {
-      operators.append(scene.constructOperator({
-        operatorName: 'useProgramOp',
-        srcFile: 'FABRIC_ROOT/SceneGraph/Resources/KL/loadShader.kl',
-        entryFunctionName: 'useProgram',
-        parameterBinding: [
-          (options.separateShaderNode ? 'shader.program' : 'self.program')
-        ]
-      }));
-    }
-
-    /////////////////////////////////
 
     for (uniformName in options.shaderUniforms) {
       uniform = options.shaderUniforms[uniformName];
@@ -595,7 +584,7 @@ FABRIC.SceneGraph.registerNodeType('Material',
         materialNode.addMemberInterface(dgnode, uniformName, true);
       }
       operatorFunction = 'load' + uniformType + 'Uniform';
-      operators.append(scene.constructOperator({
+      redrawEventHandler.preDescendBindings.append(scene.constructOperator({
         operatorName: operatorFunction + uniformName,
         srcFile: 'FABRIC_ROOT/SceneGraph/Resources/KL/loadUniforms.kl',
         preProcessorDefinitions: {
@@ -680,7 +669,6 @@ FABRIC.SceneGraph.registerNodeType('Material',
       }
     }
 
-    scene.popTimer('generatingMaterialOperators');
     return materialNode;
   });
 
