cbc47fe412660edb18ae5d6ef2d9c60ad4bbd4b1
Peter Zion <peter.zion@gmail.com>
2011-07-26 15:11:47 -0400

    Add String.refCount method in KL

--- Native/Core/CG/StringAdapter.h
+++ Native/Core/CG/StringAdapter.h
@@ -39,6 +39,7 @@ namespace Fabric
 
       void llvmReport( CG::BasicBlockBuilder &basicBlockBuilder, llvm::Value *stringRValue ) const;
 
+      llvm::Value *llvmCallRefCount( BasicBlockBuilder &basicBlockBuilder, llvm::Value *stringRValue ) const;
       llvm::Value *llvmCallLength( BasicBlockBuilder &basicBlockBuilder, llvm::Value *stringRValue ) const;
       llvm::Value *llvmCallCompare( BasicBlockBuilder &basicBlockBuilder, llvm::Value *lhsRValue, llvm::Value *rhsRValue ) const;
       llvm::Value *llvmCallCast( BasicBlockBuilder &basicBlockBuilder, RC::ConstHandle<Adapter> const &adapter, llvm::Value *rValue ) const;
