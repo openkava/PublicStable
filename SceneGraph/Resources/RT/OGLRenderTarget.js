69ba6948c754714ae9c7b86dbcd82349f4e4f51f
Phil Taylor <phil.taylor@fabric-engine.com>
2011-07-29 10:01:46 -0400

    * Cleaned up mess in repo
    * OGLRenderTargets compiling an dcode complete

--- SceneGraph/Resources/RT/OGLRenderTarget.js
+++ SceneGraph/Resources/RT/OGLRenderTarget.js
@@ -11,7 +11,7 @@
  */
 FABRIC.RT.OGLRenderTargetTextureDesc = function(type, texture) {
   this.type = type ? type : 0;
-  this.texture = texture ? texture : 0;
+  this.texture = texture ? texture : new FABRIC.RT.OGLTexture2D();
 };
 
 FABRIC.appendOnCreateContextCallback(function(context) {
@@ -31,38 +31,58 @@ FABRIC.appendOnCreateContextCallback(function(context) {
  * @param {width} start A Vec3 defining the start of the ray.
  * @param {height} direction A Vec3 defining the direction of the ray.
  */
-FABRIC.RT.OGLRenderTarget = function(width, height, textures) {
+FABRIC.RT.OGLRenderTarget = function(width, height, textures, options) {
   this.width = width ? width : 0;
   this.height = height ? height : 0;
   this.textures = textures ? textures : [];
   this.fbo = 0;
+  this.prevFbo = 0;
   this.depthBuffer = -1;
   this.hasDepthBufferTexture = false;
   this.numColorBuffers = 0;
   this.clearDepth = true;
-  this.clearColor = FABRIC.RT.rgb();
   this.clearColorFlag = true;
+  this.clearColor = (options && options.clearColor) ? options.clearColor : FABRIC.RT.rgba();
+  this.enableOptions = (options && options.enableOptions) ? options.enableOptions : 0;
+  this.cullFace = (options && options.cullFace) ? options.cullFace : 0;
   // Here we define some constants that are used to define the type
   // of buffer. We need a way of defining constants in KL.
-  this.DEPTH_BUFFER = 0;
-  this.COLOR_BUFFER = 1;
+  this.DEPTH_BUFFER = 1;
+  this.COLOR_BUFFER = 2;
 };
 
 FABRIC.appendOnCreateContextCallback(function(context) {
-  context.RegisteredTypesManager.registerType('RenderTarget', {
+  context.RegisteredTypesManager.registerType('OGLRenderTarget', {
     members: {
-      textures: 'OGLTexture2D[]',
       width: 'Size',
       height: 'Size',
+      textures: 'OGLRenderTargetTextureDesc[]',
       fbo: 'Size',
+      prevFbo: 'Size',
       depthBuffer: 'Integer',
       hasDepthBufferTexture: 'Boolean',
       numColorBuffers: 'Integer',
       clearDepth: 'Boolean',
-      clearColor: 'Boolean'
+      clearColorFlag: 'Boolean',
+      clearColor: 'Color',
+      enableOptions: 'Size',
+      cullFace: 'Size'
     },
     constructor: FABRIC.RT.OGLRenderTarget,
     kBindings: FABRIC.loadResourceURL('FABRIC_ROOT/SceneGraph/Resources/RT/OGLRenderTarget.kl')
   });
 });
+
+FABRIC.RT.oglDepthRenderTarget = function(size){
+  return new FABRIC.RT.OGLRenderTarget(
+    size,
+    size,
+    [new FABRIC.RT.OGLRenderTargetTextureDesc(1,FABRIC.RT.oglDepthBuffer2D())],
+    {
+      enableOptions:(FABRIC.SceneGraph.OpenGLConstants.GL_DEPTH_TEST |
+                     FABRIC.SceneGraph.OpenGLConstants.GL_CULL_FACE),
+      cullFace:FABRIC.SceneGraph.OpenGLConstants.GL_FRONT
+    }
+  )
+}
 
