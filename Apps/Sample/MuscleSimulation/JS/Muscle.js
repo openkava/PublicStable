842ed587325306a13ecbd338f10ace2a1d2f5ac9
Phil Taylor <phil.taylor@fabric-engine.com>
2011-08-23 11:36:25 -0400

    * Work on muscles

--- Apps/Sample/MuscleSimulation/JS/Muscle.js
+++ Apps/Sample/MuscleSimulation/JS/Muscle.js
@@ -28,6 +28,13 @@ FABRIC.SceneGraph.registerNodeType('Muscle', {
     dgnode.addMember('segmentEnvelopeIds', 'Vec4[]', segmentEnvelopeIds);
     dgnode.addMember('segmentEnvelopWeights', 'Vec4[]', segmentEnvelopWeights);
     
+    var key = FABRIC.Animation.bezierKeyframe;
+    contractionCurve = [];
+    contractionCurve.push( key(0.8, 0, null, FABRIC.RT.vec2(0.1, 0)) );
+    contractionCurve.push( key(1.0, 1.0, FABRIC.RT.vec2(-0.1, 0), FABRIC.RT.vec2(0.1, 0)));
+    contractionCurve.push( key(2.0, 2.0, FABRIC.RT.vec2(-0.1, 0), FABRIC.RT.vec2(0.1, 0)));
+    dgnode.addMember('contractionCurve', 'BezierKeyframe[]', contractionCurve);
+    
     muscle.getLength = function(){
       var segmentXfos = dgnode.getData('segmentXfos');
       return segmentXfos[0].tr.dist(segmentXfos[segmentXfos.length-1].tr);
@@ -43,32 +50,34 @@ FABRIC.SceneGraph.registerNodeType('Muscle', {
       dgnode.setData('segmentXfos', 0, segmentXfos);
     }
     
+    var coreDisplayNode;
     muscle.displayCore = function(){
-      var coreDisplayNode = scene.constructNode('Lines');
-      coreDisplayNode.pub.addVertexAttributeValue('vertexColors', 'Color', { genVBO:true } );
-      crossNode.pub.addUniformValue('crosssize', 'Scalar', options.size);
-      crossNode.getAttributesDGNode().addDependency(dgnode, 'muscles')
-      crossNode.setGeneratorOps([
-        scene.constructOperator({
-          operatorName: 'setMuscleCoreVertexCount',
-          srcFile: './KL/muscle.kl',
-          entryFunctionName: 'setMuscleCoreVertexCount',
-          parameterLayout: [
-            'muscles.segmentXfos[]',
-            'self.indices',
-            'self.newCount'
-          ]
-        }),
-        scene.constructOperator({
-          operatorName: 'fitMuscleCoreToMuscleXfos',
-          srcFile: './KL/muscle.kl',
-          entryFunctionName: 'fitMuscleCoreToMuscleXfos',
-          parameterLayout: [
-            'muscles.segmentXfos',
-            'self.positions[]'
-          ]
-        })
-      ]);
+      if(!coreDisplayNode){
+        coreDisplayNode = scene.constructNode('Lines');
+        coreDisplayNode.pub.addVertexAttributeValue('vertexColors', 'Color', { genVBO:true } );
+        coreDisplayNode.getAttributesDGNode().addDependency(dgnode, 'muscles')
+        coreDisplayNode.setGeneratorOps([
+          scene.constructOperator({
+            operatorName: 'setMuscleCoreVertexCount',
+            srcFile: './KL/muscle.kl',
+            entryFunctionName: 'setMuscleCoreVertexCount',
+            parameterLayout: [
+              'muscles.segmentXfos[]',
+              'self.indices',
+              'self.newCount'
+            ]
+          }),
+          scene.constructOperator({
+            operatorName: 'fitMuscleCoreToMuscleXfos',
+            srcFile: './KL/muscle.kl',
+            entryFunctionName: 'fitMuscleCoreToMuscleXfos',
+            parameterLayout: [
+              'muscles.segmentXfos',
+              'self.positions[]'
+            ]
+          })
+        ]);
+      }
     }
   }});
 
