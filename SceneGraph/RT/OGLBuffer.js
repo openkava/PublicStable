f4e0b599e94a5b0f331c85dfce66cfad1825d299
Phil Taylor <phil.taylor@fabric-engine.com>
2011-08-31 00:23:08 -0400

    * Started working on new indices buffer type but stopped.  - It looks like a good plan, but needs more thought.

--- SceneGraph/RT/OGLBuffer.js
+++ SceneGraph/RT/OGLBuffer.js
@@ -21,15 +21,7 @@ FABRIC.RT.OGLBuffer = function(name, id, dataType, dynamic) {
   this.reload = false;
   this.elementCount = 0;
   this.elementDataSize = dataType ? dataType.size : 0;
-  this.numBufferElementComponents = this.elementDataSize/4; // TODO: review this member.
-  if(this.numBufferElementComponents > 4){
-    // We are currenlty storing vertex weights and bone indices for defomation
-    // in mat33 values. While it seems to work, openGL buffers are only supposed
-    // to be able to store up to 16 bytes per vertex. Interestingly its been working
-    // on all GPUs. Note: We should probably split the vertex bones weights/indices
-    //  into 2 vec4s instead of one mat33.
-    this.numBufferElementComponents = 3;
-  }
+  this.numBufferElementComponents = this.elementDataSize/4;
   this.bufferType = FABRIC.SceneGraph.OpenGLConstants ? FABRIC.SceneGraph.OpenGLConstants.GL_ARRAY_BUFFER : 0;
   this.bufferUsage = FABRIC.SceneGraph.OpenGLConstants ? FABRIC.SceneGraph.OpenGLConstants.GL_STATIC_DRAW : 0;
   this.bufferElementComponentType = FABRIC.SceneGraph.OpenGLConstants ? FABRIC.SceneGraph.OpenGLConstants.GL_FLOAT : 0;
@@ -56,5 +48,38 @@ FABRIC.appendOnCreateContextCallback(function(context) {
   });
 });
 
+
+
+
+/**
+ * Struct to store information about an opernGL buffer (VBO)
+ * @constructor
+ * @param {string} name The name of the VBO
+ * @param {string} id The id of the VBO.
+ * @param {string} dataType The state of the shader's value.
+ */
+/* A single set of indices can be used to generate multiple buffers. */
+/* e.g. a set of indices for triangles can be usded to generate adjacency lists. */
+FABRIC.RT.OGLIndicesBuffer = function(name, id, dataType, dynamic) {
+  this.name = (name !== undefined) ? name : '';
+  this.attributeID = (id !== undefined) ? id : 0;
+  this.bufferIDs = []; 
+  this.reload = false;
+  this.elementCount = 0;
+};
+
+
+FABRIC.appendOnCreateContextCallback(function(context) {
+  context.RegisteredTypesManager.registerType('OGLIndicesBuffer', {
+    members: {
+      attributeID: 'Integer',
+      bufferIDs: 'Integer[]',
+      reload: 'Boolean',
+      elementCount: 'Integer'
+    },
+    constructor: FABRIC.RT.OGLIndicesBuffer,
+    kBindings: FABRIC.loadResourceURL('FABRIC_ROOT/SceneGraph/RT/OGLIndicesBuffer.kl')
+  });
+});
 
 
