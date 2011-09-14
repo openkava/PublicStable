a03c5a5949429a7bc7e4f09fc8c918b5948d1378
Phil Taylor <phil.taylor@fabric-engine.com>
2011-09-14 09:13:10 -0400

    Revert "Revert "Make FABRIC.Wrappers.js a string that is exported from the core""
    This reverts commit 2e0b7dc920b492061437956b7939f728b8d20555.

--- Core/FABRIC.js
+++ Core/FABRIC.js
@@ -48,6 +48,7 @@ FABRIC = (function() {
     //embedTag.style.display = 'none';
     document.body.appendChild(embedTag);
     
+    var wrapFabricClient = eval(embedTag.wrapFabricClientJSSource);
     var context = wrapFabricClient(embedTag, function(s) { console.log(s); } );
     
     ///////////////////////////////////////////////////////////
