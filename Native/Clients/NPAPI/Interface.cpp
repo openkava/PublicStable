89b624ea15bc007351e937ada5ed576661f4a2f8
Phil Taylor <phil.taylor@fabric-engine.com>
2011-09-14 09:09:44 -0400

    Revert "Make FABRIC.Wrappers.js a string that is exported from the core"
    This reverts commit 429a023656af2fd262420d7902c3f41ae7ade249.

--- Native/Clients/NPAPI/Interface.cpp
+++ Native/Clients/NPAPI/Interface.cpp
@@ -27,7 +27,6 @@ namespace Fabric
       NPInterfaceObject()
         : m_jsonExecNPIdentifier( NPN_GetStringIdentifier( "jsonExec" ) )
         , m_setJSONNotifyCallbackNPIdentifier( NPN_GetStringIdentifier( "setJSONNotifyCallback" ) )
-        , m_wrapFabricClientJSSourceNPIdentifier( NPN_GetStringIdentifier( "wrapFabricClientJSSource" ) )
       {
       }
       
@@ -150,47 +149,14 @@ namespace Fabric
         return false;
       }
       
-      bool npHasProperty( NPIdentifier identifier ) const
-      {
-        if ( identifier == m_wrapFabricClientJSSourceNPIdentifier )
-          return true;
-        else return false;
-      }
-      
       static bool NPHasProperty( NPObject *npObject, NPIdentifier identifier )
       {
-        return static_cast<NPInterfaceObject *>( npObject )->npHasProperty( identifier );
+        return false;
       }
 
-      bool npGetProperty( NPIdentifier identifier, NPVariant *result )
+      static bool NPGetProperty( NPObject *npobj, NPIdentifier name, NPVariant *result )
       {
-        if ( identifier == m_wrapFabricClientJSSourceNPIdentifier )
-        {
-          try
-          {
-            std::string const &wrapFabricClientJSSource = DG::Context::GetWrapFabricClientJSSource();
-            
-            size_t length = wrapFabricClientJSSource.length();
-            NPUTF8 *data = static_cast<NPUTF8 *>( NPN_MemAlloc( length + 1 ) );
-            memcpy( data, wrapFabricClientJSSource.c_str(), length+1 );
-            STRINGN_TO_NPVARIANT( data, length, *result );
-          }
-          catch ( Exception e )
-          {
-            NPN_SetException( this, (const char*)e.getDesc() );
-          }
-          catch ( ... )
-          {
-            NPN_SetException( this, "generic exception" );
-          }
-          return true;
-        }
-        else return false;
-      }
-
-      static bool NPGetProperty( NPObject *npObject, NPIdentifier identifier, NPVariant *result )
-      {
-        return static_cast<NPInterfaceObject *>( npObject )->npGetProperty( identifier, result );
+        return false;
       }
       
       static bool NPSetProperty( NPObject *npobj, NPIdentifier name, const NPVariant *value )
@@ -228,7 +194,6 @@ namespace Fabric
       
       NPIdentifier m_jsonExecNPIdentifier;
       NPIdentifier m_setJSONNotifyCallbackNPIdentifier;
-      NPIdentifier m_wrapFabricClientJSSourceNPIdentifier;
       
       static NPClass s_npClass;
     };
