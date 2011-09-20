a5c47dc53030e6c11690913eba7778a13880a0bf
Helge Mathee <helge.mathee@fabric-engine.com>
2011-09-20 18:20:04 +0200

    case-38: added bullet kl and js types as well scenegraphnodes

--- Native/Exts/Builtin/BULLET/bullet.cpp
+++ Native/Exts/Builtin/BULLET/bullet.cpp
@@ -18,6 +18,7 @@
 
 using namespace Fabric::EDK;
 IMPLEMENT_FABRIC_EDK_ENTRIES
+
 const int maxProxies = 32766;
 const int maxOverlap = 65535;
 
@@ -31,8 +32,18 @@ struct fabricBulletWorld {
   btSoftBodyWorldInfo	mSoftBodyWorldInfo;
 };
 
-FABRIC_EXT_EXPORT void FabricBULLET_CreateWorld(
-  KL::Data worldData
+struct fabricBulletShape {
+	btCollisionShape * mShape;
+};
+
+struct fabricBulletRigidBody {
+  fabricBulletShape * mShape;
+	btRigidBody * mBody;
+};
+
+// implement the callbacks
+FABRIC_EXT_EXPORT void FabricBULLET_World_Create(
+  KL::Data & worldData
 )
 {
   if(worldData == NULL) {
@@ -54,11 +65,13 @@ FABRIC_EXT_EXPORT void FabricBULLET_CreateWorld(
     btGImpactCollisionAlgorithm::registerAlgorithm(world->mDispatcher);
     world->mSoftBodyWorldInfo.m_sparsesdf.Initialize();
     world->mDynamicsWorld->getDispatchInfo().m_enableSPU = true;
+    
+    printf("   { FabricBULLET } Created Simulation World.\n");
   }
 }
 
-FABRIC_EXT_EXPORT void FabricBULLET_DeleteWorld(
-  KL::Data worldData
+FABRIC_EXT_EXPORT void FabricBULLET_World_Delete(
+  KL::Data & worldData
 )
 {
   if(worldData != NULL) {
@@ -71,5 +84,311 @@ FABRIC_EXT_EXPORT void FabricBULLET_DeleteWorld(
     delete( world->mDispatcher );
     delete( world->mCollisionConfiguration );
     delete( world );
+
+    printf("   { FabricBULLET } Deleted Simulation World.\n");
   }
 }
+
+FABRIC_EXT_EXPORT void FabricBULLET_World_SetGravity(
+  KL::Data & worldData,
+  KL::Vec3 & gravity
+)
+{
+  if(worldData != NULL) {
+    fabricBulletWorld * world = (fabricBulletWorld *)worldData;
+    world->mDynamicsWorld->setGravity(btVector3(gravity.x,gravity.y,gravity.z));
+  }
+}
+
+FABRIC_EXT_EXPORT void FabricBULLET_World_Step(
+  KL::Data & worldData,
+  KL::Size & worldStep,
+  KL::Size & worldSubsteps,
+  KL::Scalar & timeStep
+)
+{
+  if(worldData != NULL) {
+    fabricBulletWorld * world = (fabricBulletWorld *)worldData;
+    KL::Scalar timeSubstep = timeStep / KL::Scalar(worldSubsteps);
+    for(KL::Size step=0; step<worldSubsteps; step++, worldStep++) {
+      world->mDynamicsWorld->stepSimulation(timeSubstep,0,timeStep);
+    }
+  }
+}
+
+FABRIC_EXT_EXPORT void FabricBULLET_World_AddRigidBody(
+  KL::Data & worldData,
+  KL::Data & bodyData
+)
+{
+  if(worldData != NULL && bodyData != NULL) {
+    fabricBulletWorld * world = (fabricBulletWorld *)worldData;
+    fabricBulletRigidBody * body = (fabricBulletRigidBody *)bodyData;
+    world->mDynamicsWorld->addRigidBody(body->mBody);
+  }
+}
+
+FABRIC_EXT_EXPORT void FabricBULLET_World_RemoveRigidBody(
+  KL::Data & worldData,
+  KL::Data & bodyData
+)
+{
+  if(worldData != NULL && bodyData != NULL) {
+    fabricBulletWorld * world = (fabricBulletWorld *)worldData;
+    fabricBulletRigidBody * body = (fabricBulletRigidBody *)bodyData;
+    world->mDynamicsWorld->removeRigidBody(body->mBody);
+  }
+}
+
+FABRIC_EXT_EXPORT void FabricBULLET_World_Reset(
+  KL::Data & worldData,
+  KL::Size & worldStep
+)
+{
+  if(worldData != NULL) {
+    fabricBulletWorld * world = (fabricBulletWorld *)worldData;
+    worldStep = 0;
+  }
+}
+
+// implement the callbacks
+FABRIC_EXT_EXPORT void FabricBULLET_Shape_Create(
+  KL::Data & shapeData,
+  KL::Integer & shapeType,
+  KL::VariableArray<KL::Scalar> & shapeParameters,
+  KL::VariableArray<KL::Vec3> & shapeVertices
+)
+{
+  if(shapeData == NULL) {
+    
+    // validate the shape type first
+    btCollisionShape * collisionShape = NULL;
+    if(shapeType == BOX_SHAPE_PROXYTYPE) {
+      
+      if(shapeParameters.size() != 3) {
+        printf("   { FabricBULLET } ERROR: For the box shape you need to specify three parameters.\n");
+        return;
+      }
+      collisionShape = new btBoxShape(btVector3(shapeParameters[0],shapeParameters[1],shapeParameters[2]));
+      
+    //} else if(shapeType == CONVEX_HULL_SHAPE_PROXYTYPE) {
+    } else if(shapeType == SPHERE_SHAPE_PROXYTYPE) {
+
+      if(shapeParameters.size() != 1) {
+        printf("   { FabricBULLET } ERROR: For the sphere shape you need to specify one parameter.\n");
+        return;
+      }
+      collisionShape = new btSphereShape(shapeParameters[0]);
+
+    //} else if(shapeType == CAPSULE_SHAPE_PROXYTYPE) {
+    //} else if(shapeType == CONE_SHAPE_PROXYTYPE) {
+    //} else if(shapeType == CYLINDER_SHAPE_PROXYTYPE) {
+    //} else if(shapeType == GIMPACT_SHAPE_PROXYTYPE) {
+    } else if(shapeType == STATIC_PLANE_PROXYTYPE) {
+
+      if(shapeParameters.size() != 4) {
+        printf("   { FabricBULLET } ERROR: For the plane shape you need to specify four parameters.\n");
+        return;
+      }
+      collisionShape = new btStaticPlaneShape(btVector3(shapeParameters[0],shapeParameters[1],shapeParameters[2]),shapeParameters[3]);
+      
+    //} else if(shapeType == COMPOUND_SHAPE_PROXYTYPE) {
+    //} else if(shapeType == SOFTBODY_SHAPE_PROXYTYPE) {
+    }
+    
+    // if we don't have a shape, we can't do this
+    if(collisionShape == NULL) {
+      printf("   { FabricBULLET } ERROR: For the shape type %d is not supported.\n",int(shapeType));
+      return;
+    }
+
+    fabricBulletShape * shape = new fabricBulletShape();
+    shapeData = shape;
+    shape->mShape = collisionShape;
+    
+    printf("   { FabricBULLET } Created Shape of type %d.\n",int(shapeType));
+  }
+}
+
+FABRIC_EXT_EXPORT void FabricBULLET_Shape_Delete(
+  KL::Data & shapeData
+)
+{
+  if(shapeData != NULL) {
+    fabricBulletShape * shape = (fabricBulletShape *)shapeData;
+    shapeData = NULL;
+
+    delete( shape->mShape );
+    delete( shape );
+
+    printf("   { FabricBULLET } Deleted Shape.\n");
+  }
+}
+
+FABRIC_EXT_EXPORT void FabricBULLET_RigidBody_Create(
+  KL::Data & bodyData,
+  KL::Data & shapeData,
+  KL::Xfo & bodyTransform,
+  KL::Scalar & bodyMass,
+  KL::Scalar & bodyFriction,
+  KL::Scalar & bodyRestitution
+)
+{
+  if(bodyData == NULL) {
+    if(shapeData == NULL) {
+      printf("   { FabricBULLET } ERROR: Cannot create a RigidBody with a NULL shape.\n");
+      return;
+    }
+    
+    fabricBulletShape * shape = (fabricBulletShape *)shapeData;
+
+    // convert the transform.
+    btTransform transform;
+    transform.setOrigin(btVector3(bodyTransform.tr.x,bodyTransform.tr.y,bodyTransform.tr.z));
+    transform.setRotation(btQuaternion(bodyTransform.ori.v.x,bodyTransform.ori.v.y,bodyTransform.ori.v.z,bodyTransform.ori.w));
+    btMotionState* motionState = new btDefaultMotionState(transform);
+
+    btVector3 inertia(0,0,0);
+    if(bodyMass > 0.0f)
+      shape->mShape->calculateLocalInertia(bodyMass,inertia);
+      
+    shape->mShape->setLocalScaling(btVector3(bodyTransform.sc.x,bodyTransform.sc.y,bodyTransform.sc.z));
+
+    btRigidBody::btRigidBodyConstructionInfo info(bodyMass,motionState,shape->mShape,inertia);
+    info.m_friction = bodyFriction;
+    info.m_restitution = bodyRestitution;
+    
+    fabricBulletRigidBody * body = new fabricBulletRigidBody();
+    bodyData = body;
+    
+    body->mBody = new btRigidBody(info);
+    body->mShape = shape;
+
+    if(bodyMass == 0.0f)
+      body->mBody->setCollisionFlags( body->mBody->getCollisionFlags() | btCollisionObject::CF_KINEMATIC_OBJECT);
+    else
+      body->mBody->setCollisionFlags( body->mBody->getCollisionFlags() & ~btCollisionObject::CF_KINEMATIC_OBJECT);
+    
+    printf("   { FabricBULLET } Created RigidBody.\n");
+  }
+}
+
+FABRIC_EXT_EXPORT void FabricBULLET_RigidBody_Delete(
+  KL::Data & bodyData
+)
+{
+  if(bodyData == NULL) {
+    fabricBulletRigidBody * body = (fabricBulletRigidBody * )bodyData;
+    delete(body);
+    bodyData = NULL;
+
+    printf("   { FabricBULLET } Deleted RigidBody.\n");
+  }  
+}
+
+FABRIC_EXT_EXPORT void FabricBULLET_RigidBody_SetMass(
+  KL::Data & bodyData,
+  KL::Scalar & bodyMass
+)
+{
+  if(bodyData != NULL) {
+    fabricBulletRigidBody * body = (fabricBulletRigidBody * )bodyData;
+    btVector3 inertia(0,0,0);
+    if(bodyMass > 0.0f)
+       body->mBody->getCollisionShape()->calculateLocalInertia(bodyMass,inertia);
+    body->mBody->setMassProps(bodyMass,inertia);
+    if(bodyMass == 0.0f)
+      body->mBody->setCollisionFlags( body->mBody->getCollisionFlags() | btCollisionObject::CF_KINEMATIC_OBJECT);
+    else
+      body->mBody->setCollisionFlags( body->mBody->getCollisionFlags() & ~btCollisionObject::CF_KINEMATIC_OBJECT);
+  }  
+}
+
+FABRIC_EXT_EXPORT void FabricBULLET_RigidBody_GetTransform(
+  KL::Data & bodyData,
+  KL::Xfo & bodyTransform
+)
+{
+  if(bodyData != NULL) {
+    fabricBulletRigidBody * body = (fabricBulletRigidBody * )bodyData;
+    btTransform & transform = body->mBody->getWorldTransform();
+    bodyTransform.tr.x = transform.getOrigin().getX();
+    bodyTransform.tr.y = transform.getOrigin().getY();
+    bodyTransform.tr.z = transform.getOrigin().getZ();
+    bodyTransform.ori.v.x = transform.getRotation().getX();
+    bodyTransform.ori.v.y = transform.getRotation().getY();
+    bodyTransform.ori.v.z = transform.getRotation().getZ();
+    bodyTransform.ori.w = transform.getRotation().getW();
+    bodyTransform.sc.x = body->mShape->mShape->getLocalScaling().getX();
+    bodyTransform.sc.y = body->mShape->mShape->getLocalScaling().getY();
+    bodyTransform.sc.z = body->mShape->mShape->getLocalScaling().getZ();
+  }  
+}
+
+FABRIC_EXT_EXPORT void FabricBULLET_RigidBody_SetTransform(
+  KL::Data & bodyData,
+  KL::Xfo & bodyTransform
+)
+{
+  if(bodyData != NULL) {
+    fabricBulletRigidBody * body = (fabricBulletRigidBody * )bodyData;
+    if(body->mBody->getInvMass() == 0.0f) {
+      btTransform transform;
+      transform.setOrigin(btVector3(bodyTransform.tr.x,bodyTransform.tr.y,bodyTransform.tr.z));
+      transform.setRotation(btQuaternion(bodyTransform.ori.v.x,bodyTransform.ori.v.y,bodyTransform.ori.v.z,bodyTransform.ori.w));
+      body->mBody->setWorldTransform(transform);
+      body->mShape->mShape->setLocalScaling(btVector3(bodyTransform.sc.x,bodyTransform.sc.y,bodyTransform.sc.z));
+    }
+  }  
+}
+
+FABRIC_EXT_EXPORT void FabricBULLET_RigidBody_GetLinearVelocity(
+  KL::Data & bodyData,
+  KL::Vec3 & bodyVelocity
+)
+{
+  if(bodyData != NULL) {
+    fabricBulletRigidBody * body = (fabricBulletRigidBody * )bodyData;
+    btVector3 velocity = body->mBody->getLinearVelocity();
+    bodyVelocity.x = velocity.getX();
+    bodyVelocity.y = velocity.getY();
+    bodyVelocity.z = velocity.getZ();
+  }  
+}
+
+FABRIC_EXT_EXPORT void FabricBULLET_RigidBody_SetLinearVelocity(
+  KL::Data & bodyData,
+  KL::Vec3 & bodyVelocity
+)
+{
+  if(bodyData != NULL) {
+    fabricBulletRigidBody * body = (fabricBulletRigidBody * )bodyData;
+    body->mBody->setLinearVelocity(btVector3(bodyVelocity.x,bodyVelocity.y,bodyVelocity.z));
+  }  
+}
+
+FABRIC_EXT_EXPORT void FabricBULLET_RigidBody_GetAngularVelocity(
+  KL::Data & bodyData,
+  KL::Vec3 & bodyVelocity
+)
+{
+  if(bodyData != NULL) {
+    fabricBulletRigidBody * body = (fabricBulletRigidBody * )bodyData;
+    btVector3 velocity = body->mBody->getAngularVelocity();
+    bodyVelocity.x = velocity.getX();
+    bodyVelocity.y = velocity.getY();
+    bodyVelocity.z = velocity.getZ();
+  }  
+}
+
+FABRIC_EXT_EXPORT void FabricBULLET_RigidBody_SetAngularVelocity(
+  KL::Data & bodyData,
+  KL::Vec3 & bodyVelocity
+)
+{
+  if(bodyData != NULL) {
+    fabricBulletRigidBody * body = (fabricBulletRigidBody * )bodyData;
+    body->mBody->setAngularVelocity(btVector3(bodyVelocity.x,bodyVelocity.y,bodyVelocity.z));
+  }  
+}
+
