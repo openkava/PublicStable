cdeffd4e03bcae34665fca862065599d1e8196e4
Jerome Couture-Gagnon <jerome.couture-gagnon@fabric-engine.com>
2011-09-28 08:39:56 -0400

    Method & tests for homogeneous Vec4 -> Vec3

--- Native/Test/RT/testMat.js
+++ Native/Test/RT/testMat.js
@@ -31,6 +31,17 @@ dimSpecificCodePrefix[4] =
   'xAxis.set(1.0,0.0,0.0,0.0);\n' +
   'yAxis.set(0.0,1.0,0.0,0.0);\n';
 
+var dimSpecificCodeTests = [];
+dimSpecificCodeTests[2] = '';
+dimSpecificCodeTests[3] = '';
+dimSpecificCodeTests[4] = 
+    'v1.set(1.0,2.0,3.0,1.0);\n' +
+    'v1 = m1.multiplyVector(v1);\n' +
+    'v1.appendResult(tests, results, \'multiplyVec4\');\n' +
+    'v2.set(1.0,2.0,3.0);\n' +
+    'v2 = m1.multiplyVector(v2);\n' +
+    'v2.appendResult(tests, results, \'multiplyVec3\');\n';
+
 for( var dim = 2; dim <= 4; ++dim ) {
   var type = ('Mat' + dim) + dim;
   var vecType = 'Vec' + dim;
@@ -39,10 +50,15 @@ for( var dim = 2; dim <= 4; ++dim ) {
   appendKLOpAdaptors(type, [ '+', '+=', '-', '-=', '*', '*=', ['*','Scalar'], ['*=','Scalar'], ['/','Scalar'], ['/=','Scalar'] ] );
   //Add special adaptor for function VecX MatX.multiplyVecX( in VecX other)
   appendToKLCode(type, "\nfunction " + vecType + ' ' + type + ".multiplyVector( in " + vecType + " other ) {\n  return this * other;\n}\n\n");
+  var vecType2 = vecType;
+  if(dim == 4) {
+    vecType2 = 'Vec3';
+    appendToKLCode(type, "\nfunction " + vecType2 + ' ' + type + ".multiplyVector( in " + vecType2 + " other ) {\n  return this * other;\n}\n\n");
+  }
   loadType( type );
   defineInPlaceOpAdaptors(type, [ '+=', '-=', '*=', [ '*=', 'Scalar' ], [ '/=', 'Scalar' ] ] );
 
-  runTests( type, [[type,'m1'], [type,'XtoY'], [type,'YtoMinusY'], [type,'res'], ['Scalar','s1'], [vecType,'v1'], [vecType,'xAxis'], [vecType,'yAxis']], dimSpecificCodePrefix[dim] + testCode );
+  runTests( type, [[type,'m1'], [type,'XtoY'], [type,'YtoMinusY'], [type,'res'], ['Scalar','s1'], [vecType,'v1'], [vecType2,'v2'], [vecType,'xAxis'], [vecType,'yAxis']], dimSpecificCodePrefix[dim] + testCode + dimSpecificCodeTests[dim] );
 }
 
 F.flush();
