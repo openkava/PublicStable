c4e11bbe2d390a92fca2c56e7e15e1705b52e15f
Jerome Couture-Gagnon <jerome.couture-gagnon@fabric-engine.com>
2011-09-27 22:46:56 -0400

    Matrix unit tests & cleanup

--- Native/Test/RT/testVec.js
+++ Native/Test/RT/testVec.js
@@ -9,26 +9,26 @@ load( "unitTestUtils.js.inc" );
 load( "../../../Web/SceneGraph/RT/Math.js" );
 Math.verboseLogFunction = print;
 
-var vecXCode = loadTestFile( 'Vec' );
+var testCode = loadTestFile( 'Vec' );
 
 var dimSpecificCodePrefix = [];
 dimSpecificCodePrefix[2] =
   'v1.set(1.0,4.0);\n' +
-  'v2.set(2.0,1.0);';
+  'v2.set(2.0,1.0);\n';
 dimSpecificCodePrefix[3] =
   'v1.set(1.0,2.0,3.0);\n' +
-  'v2.set(2.0,2.0,1.0);';
+  'v2.set(2.0,2.0,1.0);\n';
 dimSpecificCodePrefix[4] =
   'v1.set(1.0,2.0,3.0,4.0);\n' +
-  'v2.set(2.0,2.0,1.0,1.0);';
+  'v2.set(2.0,2.0,1.0,1.0);\n';
 
 var dimSpecificCodeTests = [];
 dimSpecificCodeTests[2] =
   's1 = v1.cross(v2);\n' +
   'v1.appendScalarResult(tests, results, \'cross\', s1);';
 dimSpecificCodeTests[3] =
-  'v3 = v1.cross(v2);\n' +
-  'v3.appendResult(tests, results, \'cross\');';
+  'res = v1.cross(v2);\n' +
+  'res.appendResult(tests, results, \'cross\');';
 dimSpecificCodeTests[4] = '';
 
 for( var dim = 2; dim <= 4; ++dim ) {
@@ -38,7 +38,7 @@ for( var dim = 2; dim <= 4; ++dim ) {
   loadType( type );
   defineInPlaceOpAdaptors(type, [ '+=', '-=', '*=', [ '*=', 'Scalar' ], '/=', [ '/=', 'Scalar' ] ] );
 
-  runTests( type, [[type,'v1'], [type,'v2'], [type,'v3'], ['Scalar','s1']], dimSpecificCodePrefix[dim] + vecXCode + dimSpecificCodeTests[dim] );
+  runTests( type, [[type,'v1'], [type,'v2'], [type,'res'], ['Scalar','s1']], dimSpecificCodePrefix[dim] + testCode + dimSpecificCodeTests[dim] );
 }
 
 F.flush();
