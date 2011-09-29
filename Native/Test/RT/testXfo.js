b0d6c25f99b7405007fa0fe1da2bfc6d0cc32b4b
Jerome Couture-Gagnon <jerome.couture-gagnon@fabric-engine.com>
2011-09-29 15:49:23 -0400

    More Xfo fixes

--- Native/Test/RT/testXfo.js
+++ Native/Test/RT/testXfo.js
@@ -12,13 +12,18 @@ loadType( 'Mat33' );
 loadType( 'Vec4' );
 appendToKLCode('Mat44', "\nfunction Vec4 Mat44.multiplyVector( in Vec4 other ) {\n  return this * other;\n}\n\n");
 appendToKLCode('Mat44', "\nfunction Vec3 Mat44.multiplyVector( in Vec3 other ) {\n  return this * other;\n}\n\n");
+appendKLOpAdaptors('Mat44', [ '*', '*=' ] );
 loadType( 'Mat44' );
+defineInPlaceOpAdaptors('Mat44', [ '*=' ] );
 appendKLOpAdaptors('Euler', []);
 loadType( 'Euler' );
 loadType( 'Quat' );
-loadType( 'Xfo' );
 
-runTests( 'Xfo', [['Xfo','xfo1'], ['Xfo','xfo2'], ['Xfo','res'], ['RotationOrder','ro1'], ['Euler','e1'], ['Quat','q1'], ['Mat44','m1'], ['Vec3','v1'], ['Vec3','v2']] );
+appendKLOpAdaptors('Xfo', [ '*', '*=' ] );
+loadType( 'Xfo' );
+defineInPlaceOpAdaptors('Xfo', [ '*=' ] );
+
+runTests( 'Xfo', [['Xfo','xfo1'], ['Xfo','xfo2'], ['Xfo','res'], ['RotationOrder','ro1'], ['Euler','e1'], ['Quat','q1'], ['Mat44','m1'], ['Mat44','m2'], ['Vec3','v1'], ['Vec3','v2']] );
 
 F.flush();
 FC.dispose();
