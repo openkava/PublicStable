2708c0544cc0e4b15b1c998ad14ce138df52504b
Peter Zion <peter.zion@gmail.com>
2011-10-01 16:48:07 -0400

    Report source code locations when exceptions are fired from KL

--- Native/Core/CG/ArrayAdapter.cpp
+++ Native/Core/CG/ArrayAdapter.cpp
@@ -7,6 +7,7 @@
 #include <Fabric/Core/CG/BasicBlockBuilder.h>
 #include <Fabric/Core/CG/ConstStringAdapter.h>
 #include <Fabric/Core/CG/ExprValue.h>
+#include <Fabric/Core/CG/Location.h>
 #include <Fabric/Core/CG/SizeAdapter.h>
 #include <Fabric/Core/CG/StringAdapter.h>
 #include <Fabric/Core/RT/ArrayDesc.h>
@@ -42,21 +43,40 @@ namespace Fabric
       RC::ConstHandle<StringAdapter> const &stringAdapter,
       RC::ConstHandle<SizeAdapter> const &sizeAdapter,
       llvm::Value *indexRValue,
-      llvm::Value *sizeRValue
+      llvm::Value *sizeRValue,
+      llvm::Value *errorDescConstant
       ) const
     {
       RC::Handle<Context> context = basicBlockBuilder.getContext();
+
+      llvm::Value *errorMsg0RValue = stringAdapter->llvmCast(
+        basicBlockBuilder,
+        ExprValue( constStringAdapter, USAGE_RVALUE, context,
+          errorDescConstant? errorDescConstant: constStringAdapter->llvmConst( basicBlockBuilder, "KL" )
+          )
+        );
       
-      std::string errorMsg1 = "KL: "+getUserName()+" "+item+" (";
-      llvm::Value *errorMsg1RValue = stringAdapter->llvmCast( basicBlockBuilder,
+      std::string errorMsg1 = ": "+getUserName()+" "+item+" (";
+      llvm::Value *_errorMsg1RValue = stringAdapter->llvmCast( basicBlockBuilder,
         ExprValue( constStringAdapter, USAGE_RVALUE, context, constStringAdapter->llvmConst( basicBlockBuilder, errorMsg1 ) )
         );
+      
+      llvm::Value *errorMsg1RValue = stringAdapter->llvmCallConcat(
+        basicBlockBuilder,
+        errorMsg0RValue,
+        _errorMsg1RValue
+        );
+      stringAdapter->llvmRelease( basicBlockBuilder, errorMsg0RValue );
+      stringAdapter->llvmRelease( basicBlockBuilder, _errorMsg1RValue );
+      
       llvm::Value *indexStringRValue = stringAdapter->llvmCast( basicBlockBuilder,
         ExprValue( sizeAdapter, USAGE_RVALUE, context, indexRValue )
         );
+        
       llvm::Value *errorMsg1PlusIndexRValue = stringAdapter->llvmCallConcat( basicBlockBuilder, errorMsg1RValue, indexStringRValue );
       stringAdapter->llvmRelease( basicBlockBuilder, errorMsg1RValue );
       stringAdapter->llvmRelease( basicBlockBuilder, indexStringRValue );
+      
       std::string errorMsg2 = ") out of range (";
       llvm::Value *errorMsg2RValue = stringAdapter->llvmCast( basicBlockBuilder,
         ExprValue( constStringAdapter, USAGE_RVALUE, context, constStringAdapter->llvmConst( basicBlockBuilder, errorMsg2 ) )
@@ -78,6 +98,17 @@ namespace Fabric
       stringAdapter->llvmRelease( basicBlockBuilder, errorMsg1PlusIndexPlusErrorMsg2PlusSizeRValue );
       stringAdapter->llvmRelease( basicBlockBuilder, errorMsg3RValue );
       llvmThrowException( basicBlockBuilder, errorMsg1PlusIndexPlusErrorMsg2PlusSizePlusErrorMsg3RValue );
+    }
+
+    llvm::Value *ArrayAdapter::llvmLocationConstStringRValue(
+      BasicBlockBuilder &basicBlockBuilder,
+      RC::ConstHandle<ConstStringAdapter> const &constStringAdapter,
+      CG::Location const &location
+      ) const
+    {
+      RC::ConstHandle<RC::String> filename = location.getFilename();
+      std::string locationString = (filename? filename->stdString(): std::string("(unknown)")) + ":" + _(location.getLine()) + ":" + _(location.getColumn());
+      return constStringAdapter->llvmConst( basicBlockBuilder, locationString );
     }
   };
 };
