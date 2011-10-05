fc6848cac45f0d9156f7c9c6d44272993adc5e17
Peter Zion <peter.zion@gmail.com>
2011-10-05 09:16:20 -0400

    Another export script fix

--- Util/export-js-scene-graph.sh
+++ Util/export-js-scene-graph.sh
@@ -3,13 +3,14 @@
 # Copyright 2010-2011 Fabric Technologies Inc. All rights reserved.
 #
 
-#git filter-branch -f --prune-empty --index-filter 'git rm -r --cached --ignore-unmatch \
-#  Native \
-#  Web/Apps/Private \
-#  Util \
-#  Fabric.xcodeproj \
-#  Makefile \
-#  README \
-#  index.html' -- --all
+git filter-branch -f --prune-empty --index-filter 'git rm -r --cached --ignore-unmatch \
+  Native \
+  Web/Apps/Private \
+  Util \
+  Fabric.xcodeproj \
+  Makefile \
+  README \
+  index.html' -- --all
 git filter-branch -f --prune-empty --index-filter \
-  'git ls-files -s | sed "s,Web/\\(.*\\)$,\\1," | GIT_INDEX_FILE=$GIT_INDEX_FILE.new git update-index --index-info && mv "$GIT_INDEX_FILE.new" "$GIT_INDEX_FILE"' -- --all
+  'git ls-files -s | sed "s,Web/\\(.*\\)$,\\1," | GIT_INDEX_FILE=$GIT_INDEX_FILE.new git update-index --index-info && mv "$GIT_INDEX_FILE.new" "$GIT_INDEX_FILE"' \
+ -- --all
