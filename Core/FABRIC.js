5e38440ef727b8b367fa1ac523bda641a46770de
Phil Taylor <phil.taylor@fabric-engine.com>
2011-10-06 14:47:12 -0400

    * FABRIC.js file now references version 1.0.13

--- Core/FABRIC.js
+++ Core/FABRIC.js
@@ -60,7 +60,7 @@ FABRIC = (function() {
     // Check the currently installed version.
     // TODO: This code will be removed once we get to the end of beta.
     var version = context.build.getPureVersion().split('.');
-    var requiredVersion = [1,0,12];
+    var requiredVersion = [1,0,13];
     var cmpVersions = function (lhs, rhs) {
       if (lhs[0] < rhs[0])
         return -1;
