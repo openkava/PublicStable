64672594eb812f31d8f51009cd55642f4e6ad838
Peter Zion <peter.zion@gmail.com>
2011-10-06 12:42:58 -0400

    Fixes to upload-demos.sh corresponding to repo refactoring

--- Util/upload-demos.sh
+++ Util/upload-demos.sh
@@ -4,4 +4,4 @@ S3CMD='s3cmd --acl-public'
 S3BUCKET='s3://demos.fabric-engine.com'
 
 $S3CMD put index.html $S3BUCKET/ \
-  && $S3CMD sync Web/ $S3BUCKET/Fabric/
+  && $S3CMD sync --delete-removed --exclude '.git/*' Web/ $S3BUCKET/Fabric/
