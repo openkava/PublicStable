2846c14e4180bac0aaaf860867b5a5fb3c915044
Peter Zion <peter.zion@gmail.com>
2011-10-14 20:40:34 -0400

    Preserve order of values added to JSON:Object

--- Native/Base/JSON/Object.h
+++ Native/Base/JSON/Object.h
@@ -18,7 +18,8 @@ namespace Fabric
     class Object : public Value
     {
       typedef std::map< std::string, RC::ConstHandle<Value> > Map;
-
+      typedef std::vector<Map::const_iterator> MapOrder;
+      
     public:
     
       static RC::Handle<Object> Create()
@@ -52,23 +53,59 @@ namespace Fabric
       
       void set( std::string const &key, RC::ConstHandle<Value> const &value )
       {
-        m_map[key] = value;
+        Map::iterator it = m_map.find( key );
+        if ( it == m_map.end() )
+          m_mapOrder.push_back( m_map.insert( Map::value_type( key, value ) ).first );
+        else it->second = value;
       }
       
-      typedef Map::const_iterator const_iterator;
+      class const_iterator
+      {
+      public:
+      
+        const_iterator( MapOrder::const_iterator const &it )
+          : m_it( it )
+        {
+        }
+      
+        const_iterator( const_iterator const &that )
+          : m_it( that.m_it )
+        {
+        }
+        
+        Map::const_iterator operator ->() const
+        {
+          return *m_it;
+        }
+        
+        void operator ++()
+        {
+          ++m_it; 
+        }
+        
+        bool operator !=( const_iterator const &that )
+        {
+          return m_it != that.m_it;
+        }
+      
+      private:
+      
+        MapOrder::const_iterator m_it;
+      };
+      
       const_iterator begin() const
       {
-        return m_map.begin();
+        return m_mapOrder.begin();
       }
       const_iterator end() const
       {
-        return m_map.end();
+        return m_mapOrder.end();
       }
       
       virtual void generate( yajl_gen yajlHandle ) const
       {
         yajl_gen_map_open( yajlHandle );
-        for ( Map::const_iterator it=m_map.begin(); it!=m_map.end(); ++it )
+        for ( const_iterator it=begin(); it!=end(); ++it )
         {
           std::string const &key = it->first;
           yajl_gen_string( yajlHandle, (const unsigned char *)key.data(), key.length() );
@@ -87,6 +124,7 @@ namespace Fabric
     private:
     
       Map m_map;
+      MapOrder m_mapOrder;
     };
   };
 };
