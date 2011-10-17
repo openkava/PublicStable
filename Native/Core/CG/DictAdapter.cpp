00d82cb374c1979bffd0406459acfaf2267a6cf4
Peter Zion <peter.zion@gmail.com>
2011-10-17 18:04:55 -0400

    KL: r-value and l-value versions of indexing operator for dictionaries

--- Native/Core/CG/DictAdapter.cpp
+++ Native/Core/CG/DictAdapter.cpp
@@ -17,7 +17,7 @@
 #include "OverloadNames.h"
 
 #include <Fabric/Core/RT/DictDesc.h>
-#include <Fabric/Core/RT/Impl.h>
+#include <Fabric/Core/RT/DictImpl.h>
 
 #include <llvm/Module.h>
 #include <llvm/Function.h>
@@ -36,6 +36,7 @@ namespace Fabric
       , m_dictDesc( dictDesc )
       , m_keyAdapter( RC::ConstHandle<ComparableAdapter>::StaticCast( manager->getAdapter( dictDesc->getKeyDesc() ) ) )
       , m_valueAdapter( manager->getAdapter( dictDesc->getValueDesc() ) )
+      , m_dictImpl( dictDesc->getImpl().ptr() )
     {
     }
     
@@ -188,11 +189,201 @@ namespace Fabric
           basicBlockBuilder->CreateRetVoid();
         }
       }
+
+      /*
+      {
+        std::vector< FunctionParam > params;
+        params.push_back( FunctionParam( "lValue", this, CG::USAGE_LVALUE ) );
+        params.push_back( FunctionParam( "keyRValue", m_keyAdapter, CG::USAGE_RVALUE ) );
+        FunctionBuilder functionBuilder( moduleBuilder, "__"+getCodeName()+"__GetLValue", ExprType( m_valueAdapter, USAGE_LVALUE ), params, false, 0, true );
+        if ( buildFunctions )
+        {
+          BasicBlockBuilder basicBlockBuilder( functionBuilder );
+
+          llvm::Value *lValue = functionBuilder[0];
+          llvm::Value *keyRValue = functionBuilder[1];
+
+          llvm::BasicBlock *entryBB = basicBlockBuilder.getFunctionBuilder().createBasicBlock( "entry" );
+          llvm::BasicBlock *resizeBB = basicBlockBuilder.getFunctionBuilder().createBasicBlock( "resize" );
+          llvm::BasicBlock *accessBB = basicBlockBuilder.getFunctionBuilder().createBasicBlock( "access" );
+
+      bits_t *bits = reinterpret_cast<bits_t *>( data );
+      if ( bits->bucketCount == 0 )
+        adjustBucketCount( bits, RT_DICT_IMPL_MINIMUM_BUCKET_COUNT );
+      size_t keyHash = m_keyImpl->hash( keyData );
+      size_t bucketIndex = keyHash & (bits->bucketCount - 1);
+      bucket_t *bucket = &bits->buckets[bucketIndex];
+      void *result = getMutable( bits, bucket, keyData, keyHash );
+      size_t newBucketCount = BucketCountForNodeCount( bits->nodeCount );
+      if ( newBucketCount > bits->bucketCount )
+        adjustBucketCount( bits, newBucketCount );
+      return result;
+
+          basicBlockBuilder->SetInsertPoint( entryBB );
+          llvm::Value *nodeCountRValue = sizeAdapter->llvmLValueToRValue(
+            basicBlockBuilder,
+            basicBlockBuilder->CreateStructGEP( lValue, 1 )
+            );
+          llvm::Value *minBucketCountRValue = llvmMinBucketCountForNodeCount( basicBlockBuilder, nodeCountRValue );
+          llvm::Value *bucketCountLValue = basicBlockBuilder->CreateStructGEP( lValue, 0 );
+          llvm::Value *oldBucketCountRValue = sizeAdapter->llvmLValueToRValue( basicBlockBuilder, bucketCountLValue );
+          basicBlockBuilder->CreateCondBr(
+            basicBlockBuilder->CreateICmpLT(
+              bucketCountLValue,
+              minBucketCountRValue
+              ),
+            resizeBB,
+            accessBB
+            );
+          
+          basicBlockBuilder->SetInsertPoint( resizeBB );
+          llvm::Value *newBucketCountRValue = llvmCallAdjustBucketCount(
+            basicBlockBuilder,
+            lValue, 
+            sizeAdapter->llvmConst( context, RT_DICT_IMPL_MINIMUM_BUCKET_COUNT )
+            );
+          basicBlockBuilder->CreateBr( accessBB );
+          
+          basicBlockBuilder->SetInsertPoint( accessBB );
+          llvm::Value *keyHashRValue = llvmHash( basicBlockBuilder, keyRValue );
+          llvm::PHINode *bucketCountRVaule = basicBlockBuilder->CreatePHI( sizeAdapter->llvmRType( context ) );
+          phi->addIncoming( oldBucketCountRValue, entryBB );
+          phi->addIncoming( newBucketCountRValue, resizeBB );
+          llvm::Value *bucketIndexRValue = basicBlockBuilder->CreateAnd(
+            keyHashRValue,
+            basicBlockBuilder->CreateSub(
+              bucketCountRVaule,
+              sizeAdapter->llvmConst( context, 1 )
+              )
+            );
+          llvm::Value *bucketsPtr = basicBlockBuilder->CreateStructGEP( lValue, 4 );
+          llvm::Value *bucketLValue = basicBlockBuilder->CreateConstGEP32_2( bucketsPtr, 0, bucketIndexRValue );
+          basicBlockBuilder->CreateRet(
+            llvmCallGetLValue_Bucket(
+              lValue,
+              bucketLValue,
+              keyRValue,
+              keyHashRValue
+              )
+            );
+        }
+      }
+      
+      {
+        std::string name = "__" + getCodeName() + "__MinBucketCountForNodeCount";
+        std::vector< FunctionParam > params;
+        params.push_back( FunctionParam( "nodeCountRValue", sizeAdapter, USAGE_RVALUE ) );
+        FunctionBuilder functionBuilder( moduleBuilder, name, ExprType(), params );
+        if ( buildFunctions )
+        {
+          llvm::Value *stringLValue = functionBuilder[0];
+          llvm::Value *rValue = functionBuilder[1];
+          BasicBlockBuilder basicBlockBuilder( functionBuilder );
+          basicBlockBuilder->SetInsertPoint( functionBuilder.createBasicBlock( "entry" ) );
+          llvm::Value *lValue = llvmRValueToLValue( basicBlockBuilder, rValue );
+          llvm::Value *stringRValue = stringAdapter->llvmCallCast( basicBlockBuilder, this, lValue );
+          stringAdapter->llvmAssign( basicBlockBuilder, stringLValue, stringRValue );
+          basicBlockBuilder->CreateRetVoid();
+        }
+      }
+
+      {
+        std::vector< FunctionParam > params;
+        params.push_back( FunctionParam( "lValue", this, CG::USAGE_LVALUE ) );
+        params.push_back( FunctionParam( "bucketLValue", this, CG::USAGE_LVALUE ) );
+        params.push_back( FunctionParam( "keyRValue", m_keyAdapter, CG::USAGE_RVALUE ) );
+        FunctionBuilder functionBuilder( moduleBuilder, "__"+getCodeName()+"__GetLValue", ExprType( m_valueAdapter, USAGE_LVALUE ), params, false, 0, true );
+        if ( buildFunctions )
+        {
+          BasicBlockBuilder basicBlockBuilder( functionBuilder );
+
+          llvm::Value *lValue = functionBuilder[0];
+          llvm::Value *keyRValue = functionBuilder[1];
+
+          llvm::BasicBlock *entryBB = basicBlockBuilder.getFunctionBuilder().createBasicBlock( "entry" );
+          llvm::BasicBlock *resizeBB = basicBlockBuilder.getFunctionBuilder().createBasicBlock( "resize" );
+          llvm::BasicBlock *accessBB = basicBlockBuilder.getFunctionBuilder().createBasicBlock( "access" );
+
+          basicBlockBuilder->SetInsertPoint( entryBB );
+          llvm::Value *oldNodeCountRValue = sizeAdapter->llvmLValueToRValue(
+            basicBlockBuilder,
+            basicBlockBuilder->CreateStructGEP( lValue, 1 )
+            );
+          basicBlockBuilder->CreateCondBr(
+            basicBlockBuilder->CreateICmpEQ(
+              basicBlockBuilder->CreateAnd(
+                oldNodeCountRValue,
+                basicBlockBuilder->CreateAdd(
+                  oldNodeCountRValue,
+                  sizeAdapter->llvmConst( context, 1 )
+                  )
+                ),
+              sizeAdapter->llvmConst( context, 0 )
+            ),
+            maybeResizeBB,
+            accessBB
+            );
+          
+          basicBlockBuilder->SetInsertPoint( maybeResizeBB );
+          llvm::Value *newBucketCountRValue = llvmMaybeResize(
+            basicBlockBuilder,
+            lValue
+            );
+          basicBlockBuilder->CreateBr( accessBB );
+          
+          basicBlockBuilder->SetInsertPoint( accessBB );
+          llvm::Value *keyHashRValue = llvmHash( basicBlockBuilder, keyRValue );
+          llvm::Value *bucketCountRVaule = sizeAdapter->llvmLValueToRValue(
+            basicBlockBuilder,
+            basicBlockBuilder->CreateStructGEP( lValue, 0 )
+            );
+          llvm::Value *bucketIndexRValue = basicBlockBuilder->CreateAnd(
+            keyHashRValue,
+            basicBlockBuilder->CreateSub(
+              bucketCountRValue,
+              sizeAdapter->llvmConst( context, 1 )
+              )
+            );
+          llvm::Value *bucketsPtr = basicBlockBuilder->CreateStructGEP( lValue, 4 );
+          llvm::Value *bucketLValue = basicBlockBuilder->CreateConstGEP32_2( bucketsPtr, 0, bucketIndexRValue );
+          
+          llvm::Value *firstNodeLValue = basicBlockBuilder->CreateLoad(
+            basicBlockBuilder->CreateStructGEP( bucketLValue, 0 )
+            );
+          basicBlockBuilder->CreateBr( loopCheckBB );
+          
+          basicBlockBuilder->SetInsertPoint( loopCheckBB );
+          llvm::PHINode *nodeLValue = basicBlockBuilder->CreatePHI( llvmNodePtrType );
+          nodeLValue->addIncoming( firstNodeLValue, accessBB );
+          nodeLValue->addIncoming( nodeLValue, loopStepBB );
+          basicBlockBuilder->CreateCondBr(
+            basicBlockBuilder->CreateIsNull( nodeLValue ),
+            loopDoneBB,
+            loopStepBB
+            );
+          
+          basicBlockBuilder->SetInsertPoint( loopStepBB );
+          llvm::Value *keyLValue = basicBlockBuilder->CreatePointerCast(
+             basicBlockBuilder->CreateConstGEP32_2(
+               basicBlockBuilder->CreateStructGEP( nodeLValue, 4 ),
+               0,
+               0
+               ),
+              m_keyImpl->
+          llvm::Value *cmpRValue = m_keyImpl->llvmCompare(
+            basicBlockBuilder,
+        }
+      }
+      */
     }
     
     void *DictAdapter::llvmResolveExternalFunction( std::string const &functionName ) const
     {
-      return Adapter::llvmResolveExternalFunction( functionName );
+      if ( functionName == "__" + getCodeName() + "_GetRValue" )
+        return (void *)&DictAdapter::GetRValue;
+      else if ( functionName == "__" + getCodeName() + "_GetLValue" )
+        return (void *)&DictAdapter::GetLValue;
+      else return Adapter::llvmResolveExternalFunction( functionName );
     }
 
     void DictAdapter::llvmRetain( CG::BasicBlockBuilder &basicBlockBuilder, llvm::Value *rValue ) const
@@ -260,6 +451,88 @@ namespace Fabric
       std::string name = methodOverloadName( "size", this );
       FunctionBuilder functionBuilder( basicBlockBuilder.getModuleBuilder(), name, ExprType( sizeAdapter, USAGE_RVALUE ), params, false );
       return basicBlockBuilder->CreateCall( functionBuilder.getLLVMFunction(), rValue );
+    }
+
+    llvm::Value *DictAdapter::llvmGetRValue(
+      CG::BasicBlockBuilder &basicBlockBuilder,
+      llvm::Value *dictRValue,
+      llvm::Value *keyRValue
+      ) const
+    {
+      RC::Handle<Context> context = basicBlockBuilder.getContext();
+      
+      std::vector< llvm::Type const * > argTypes;
+      argTypes.push_back( basicBlockBuilder->getInt8PtrTy() );
+      argTypes.push_back( llvmLType( context ) );
+      argTypes.push_back( m_keyAdapter->llvmLType( context ) );
+      llvm::FunctionType const *funcType = llvm::FunctionType::get( m_valueAdapter->llvmLType( context ), argTypes, false );
+      
+      llvm::AttributeWithIndex AWI[1];
+      AWI[0] = llvm::AttributeWithIndex::get( ~0u, llvm::Attribute::InlineHint | llvm::Attribute::NoUnwind );
+      llvm::AttrListPtr attrListPtr = llvm::AttrListPtr::get( AWI, 1 );
+      
+      llvm::Function *func = llvm::cast<llvm::Function>( basicBlockBuilder.getModuleBuilder()->getOrInsertFunction( "__"+getCodeName()+"_GetRValue", funcType, attrListPtr ) ); 
+
+      llvm::Value *dictLValue = llvmRValueToLValue( basicBlockBuilder, dictRValue );
+      llvm::Value *keyLValue = m_keyAdapter->llvmRValueToLValue( basicBlockBuilder, keyRValue );
+
+      std::vector< llvm::Value * > args;
+      args.push_back( llvmAdapterPtr( basicBlockBuilder ) );
+      args.push_back( dictLValue );
+      args.push_back( keyLValue );
+      llvm::Value *resultLValue = basicBlockBuilder->CreateCall( func, args.begin(), args.end() );
+      return m_valueAdapter->llvmLValueToRValue( basicBlockBuilder, resultLValue );
+    }
+
+    llvm::Value *DictAdapter::llvmGetLValue(
+      CG::BasicBlockBuilder &basicBlockBuilder,
+      llvm::Value *dictLValue,
+      llvm::Value *keyRValue
+      ) const
+    {
+      RC::Handle<Context> context = basicBlockBuilder.getContext();
+      
+      std::vector< llvm::Type const * > argTypes;
+      argTypes.push_back( basicBlockBuilder->getInt8PtrTy() );
+      argTypes.push_back( llvmLType( context ) );
+      argTypes.push_back( m_keyAdapter->llvmLType( context ) );
+      llvm::FunctionType const *funcType = llvm::FunctionType::get( m_valueAdapter->llvmLType( context ), argTypes, false );
+      
+      llvm::AttributeWithIndex AWI[1];
+      AWI[0] = llvm::AttributeWithIndex::get( ~0u, llvm::Attribute::InlineHint | llvm::Attribute::NoUnwind );
+      llvm::AttrListPtr attrListPtr = llvm::AttrListPtr::get( AWI, 1 );
+      
+      llvm::Function *func = llvm::cast<llvm::Function>( basicBlockBuilder.getModuleBuilder()->getOrInsertFunction( "__"+getCodeName()+"_GetLValue", funcType, attrListPtr ) ); 
+
+      llvm::Value *keyLValue = m_keyAdapter->llvmRValueToLValue( basicBlockBuilder, keyRValue );
+
+      std::vector< llvm::Value * > args;
+      args.push_back( llvmAdapterPtr( basicBlockBuilder ) );
+      args.push_back( dictLValue );
+      args.push_back( keyLValue );
+      return basicBlockBuilder->CreateCall( func, args.begin(), args.end() );
+    }
+
+    void const *DictAdapter::GetRValue( void *_dictAdapter, void const *dictLValue, void const *keyLValue )
+    {
+      DictAdapter const *dictAdapter = static_cast<DictAdapter const *>( _dictAdapter );
+      return dictAdapter->m_dictImpl->getImmutable( dictLValue, keyLValue );
+    }
+
+    void *DictAdapter::GetLValue( void *_dictAdapter, void *dictLValue, void const *keyLValue )
+    {
+      DictAdapter const *dictAdapter = static_cast<DictAdapter const *>( _dictAdapter );
+      return dictAdapter->m_dictImpl->getMutable( dictLValue, keyLValue );
+    }
+
+    RC::ConstHandle<ComparableAdapter> DictAdapter::getKeyAdapter() const
+    {
+      return m_keyAdapter;
+    }
+    
+    RC::ConstHandle<Adapter> DictAdapter::getValueAdapter() const
+    {
+      return m_valueAdapter;
     }
   };
 };
