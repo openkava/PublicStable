c9e37a1961aca7271ba37d9cc5f1df20ceeaf256
Helge Mathee <helge.mathee@fabric-engine.com>
2011-11-01 17:35:59 +0100

    kinect: basics for tilt demo

--- Native/Exts/Builtin/KINECT/kinect.cpp
+++ Native/Exts/Builtin/KINECT/kinect.cpp
@@ -13,11 +13,32 @@
 using namespace Fabric::EDK;
 IMPLEMENT_FABRIC_EDK_ENTRIES
 
+HANDLE	videoStreamHandle;
+HANDLE	depthStreamHandle;
+HANDLE	nextColorFrameEvent;
+HANDLE	nextDepthFrameEvent;
+
 // ====================================================================
 // KL structs
 FABRIC_EXT_KL_STRUCT( KinectCamera, {
+  struct LocalData{
+    DWORD initFlags;
+    HANDLE  colorStream;
+    HANDLE  depthStream;
+    HANDLE  skeletonStream;
+  };
+  
+  LocalData * localData;
   KL::Boolean initiated;
   KL::Integer tiltAngle;
+  KL::Size width;
+  KL::Size height;
+  KL::Boolean supportsColor;
+  KL::Boolean supportsDepth;
+  KL::Boolean supportsSkeleton;
+  KL::VariableArray<KL::RGB> colorData;
+  KL::VariableArray<KL::Scalar> depthData;
+  KL::VariableArray<KL::Xfo> skeletonData;
 } );
 
 FABRIC_EXT_EXPORT void FabricKINECT_Init(
@@ -27,11 +48,24 @@ FABRIC_EXT_EXPORT void FabricKINECT_Init(
 #ifndef NDEBUG
   printf("  { FabricKINECT } : FabricKINECT_Init called.\n");
 #endif
-  if(!camera.initiated)
+  if(!camera.initiated && camera.localData == NULL)
   {
-    if(NuiInitialize(NUI_INITIALIZE_FLAG_USES_COLOR) >= 0)
+    // compute the flags
+    DWORD initFlags = 0;
+    if(camera.supportsColor)
+      initFlags = initFlags | NUI_INITIALIZE_FLAG_USES_COLOR;
+    if(camera.supportsDepth)
+      initFlags = initFlags | NUI_INITIALIZE_FLAG_USES_SKELETON;
+    if(camera.supportsSkeleton)
+      initFlags = initFlags | NUI_INITIALIZE_FLAG_USES_DEPTH;
+      
+    if(NuiInitialize(initFlags) >= 0)
     {
       camera.initiated = true;
+      
+      // create the localdata
+      camera.localData = new KinectCamera::LocalData();
+      camera.localData->initFlags = initFlags;
       
       // retrieve the angle from the camera
       LONG angle;
@@ -39,6 +73,12 @@ FABRIC_EXT_EXPORT void FabricKINECT_Init(
 	camera.tiltAngle = (int)angle;
       else
 	camera.tiltAngle = 0;
+	
+      // setup the image resolutions
+      camera.width = 640;
+      camera.height = 480;
+      camera.colorData.resize(camera.width * camera.height);
+      camera.depthData.resize(camera.width * camera.height);
     }
   }
 #ifndef NDEBUG
@@ -57,6 +97,11 @@ FABRIC_EXT_EXPORT void FabricKINECT_Shutdown(
   {
     NuiShutdown();
     camera.initiated = false;
+    if(camera.localData != NULL)
+    {
+      delete(camera.localData);
+      camera.localData = NULL;
+    }
   }
 #ifndef NDEBUG
   printf("  { FabricKINECT } : FabricKINECT_Shutdown completed.\n");
@@ -82,5 +127,21 @@ FABRIC_EXT_EXPORT void FabricKINECT_Tilt(
   }
 #ifndef NDEBUG
   printf("  { FabricKINECT } : FabricKINECT_Tilt completed.\n");
+#endif
+}
+
+FABRIC_EXT_EXPORT void FabricKINECT_GetColorPixels(
+  KinectCamera & camera
+)
+{
+#ifndef NDEBUG
+  printf("  { FabricKINECT } : FabricKINECT_GetColorPixels called.\n");
+#endif
+  if(camera.initiated)
+  {
+    
+  }
+#ifndef NDEBUG
+  printf("  { FabricKINECT } : FabricKINECT_GetColorPixels completed.\n");
 #endif
 }
