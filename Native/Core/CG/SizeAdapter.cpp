ffe21201f2003a6f97ea8dc46269f891eba7a709
Peter Zion <peter.zion@gmail.com>
2011-11-02 13:06:26 -0400

    Fix leak of error messages on div/mod by zero

--- Native/Core/CG/SizeAdapter.cpp
+++ Native/Core/CG/SizeAdapter.cpp
@@ -323,6 +323,7 @@ namespace Fabric
           ExprValue errorExprValue( constStringAdapter, USAGE_RVALUE, context, constStringAdapter->llvmConst( basicBlockBuilder, errorMsg ) );
           llvm::Value *errorStringRValue = stringAdapter->llvmCast( basicBlockBuilder, errorExprValue );
           stringAdapter->llvmReport( basicBlockBuilder, errorStringRValue );
+          stringAdapter->llvmDispose( basicBlockBuilder, errorStringRValue );
           llvm::Value *defaultRValue = llvmDefaultRValue( basicBlockBuilder );
           basicBlockBuilder->CreateRet( defaultRValue );
         }
@@ -357,6 +358,7 @@ namespace Fabric
           ExprValue errorExprValue( constStringAdapter, USAGE_RVALUE, context, constStringAdapter->llvmConst( basicBlockBuilder, errorMsg ) );
           llvm::Value *errorStringRValue = stringAdapter->llvmCast( basicBlockBuilder, errorExprValue );
           stringAdapter->llvmReport( basicBlockBuilder, errorStringRValue );
+          stringAdapter->llvmDispose( basicBlockBuilder, errorStringRValue );
           llvm::Value *defaultRValue = llvmDefaultRValue( basicBlockBuilder );
           basicBlockBuilder->CreateRet( defaultRValue );
         }
