1a696c121cf3665a250d619b079ba0e6faa7a3b1
Phil Taylor <phil.taylor@fabric-engine.com>
2011-11-07 18:15:20 -0500

    * Moved evaluate key tracks to PoseVariables.

--- SceneGraph/RT/PoseVariables.js
+++ SceneGraph/RT/PoseVariables.js
@@ -1,57 +1,6 @@
 
 
 
-FABRIC.RT.KeyframeTrackBinding = function(varId, trackIds) {
-  this.varId = varId != undefined ? varId : -1;
-  this.trackIds = trackIds != undefined ? trackIds : [];
-};
-
-FABRIC.appendOnCreateContextCallback(function(context) {
-  context.RegisteredTypesManager.registerType('KeyframeTrackBinding', {
-    members: {
-      varId: 'Integer',
-      trackIds: 'Integer[]'
-    },
-    constructor: FABRIC.RT.KeyframeTrackBinding
-  });
-});
-
-
-FABRIC.RT.KeyframeTrackBindings = function() {
-  this.scalarBindings = [];
-  this.vec3Bindings = [];
-  this.quatBindings = [];
-  this.xfoBindings = [];
-};
-
-
-FABRIC.RT.KeyframeTrackBindings.prototype = {
-  addScalarBinding: function(varId, trackId){
-    this.scalarBindings.push(new FABRIC.RT.KeyframeTrackBinding(varId, [trackId]));
-  },
-  addXfoBinding: function(varId, trackIds){
-    this.xfoBindings.push(new FABRIC.RT.KeyframeTrackBinding(varId, trackIds));
-  }
-};
-
-
-FABRIC.appendOnCreateContextCallback(function(context) {
-  context.RegisteredTypesManager.registerType('KeyframeTrackBindings', {
-    members: {
-      scalarBindings: 'KeyframeTrackBinding[]',
-      vec3Bindings: 'KeyframeTrackBinding[]',
-      quatBindings: 'KeyframeTrackBinding[]',
-      xfoBindings: 'KeyframeTrackBinding[]'
-    },
-    constructor: FABRIC.RT.KeyframeTrackBindings,
-    klBindings: {
-      filename: 'KeyframeTrackBindings.kl',
-      sourceCode: FABRIC.loadResourceURL('FABRIC_ROOT/SceneGraph/RT/KeyframeTrackBindings.kl')
-    }
-  });
-});
-
-
 FABRIC.RT.PoseVariables = function() {
   this.scalarValues = [];
   this.vec3Values = [];
@@ -121,7 +70,10 @@ FABRIC.appendOnCreateContextCallback(function(context) {
     constructor: FABRIC.RT.PoseVariables,
     klBindings: {
       filename: 'PoseVariables.kl',
-      sourceCode: FABRIC.loadResourceURL('FABRIC_ROOT/SceneGraph/RT/PoseVariables.kl')
+      sourceCode: FABRIC.preProcessCode(
+        FABRIC.loadResourceURL('FABRIC_ROOT/SceneGraph/RT/PoseVariables.kl'), {
+          KEYFRAMETYPE: 'LinearKeyframe'
+        })
     }
   });
 });
