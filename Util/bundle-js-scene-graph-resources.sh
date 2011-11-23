1af5c8a20ebc62047a93bd257f7656491e1cf7c1
Helge Mathee <helge.mathee@fabric-engine.com>
2011-11-23 08:33:54 +0100

    updated bundle script for jsscenegraph resources

--- Util/bundle-js-scene-graph-resources.sh
+++ Util/bundle-js-scene-graph-resources.sh
@@ -6,6 +6,8 @@ zip -qr js-scene-graph-resources-$DATE.zip \
   Apps/Sample/BasicDemos/Models \
   Apps/Sample/BasicDemos/Resources \
   Apps/Sample/BasicDemos/Resources/cubemap_opensea \
+  Apps/Sample/Characters/Assets \
+  Apps/Sample/Rendering/Resources \
   Apps/Sample/Tutorials/Resources \
   Apps/Sample/Alembic/Models \
   Apps/Sample/Lidar/Models \
