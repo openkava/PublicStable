91339ccc8ac3e8b0dee86a748ab58bd9de708967
Phil Taylor <phil.taylor@fabric-engine.com>
2011-11-26 16:22:52 -0500

    * Added reload button to operatorEditor

--- SceneGraph/FABRIC.SceneGraph.js
+++ SceneGraph/FABRIC.SceneGraph.js
@@ -407,7 +407,13 @@ FABRIC.SceneGraph = {
       }
 
       if (operatorDef.srcFile) {
-        filename = operatorDef.srcFile.split('/').pop();
+        filename = operatorDef.srcFile;
+        // PT 26-11-2011: HACK: Ideally we could store the preprocessor definitions
+        // in a separete member. The file name is parsed in the operator editor for
+        // preprocessor definitions. (used in the reload and recompile option).
+        if(operatorDef.preProcessorDefinitions){
+          filename += "$"+JSON.stringify(operatorDef.preProcessorDefinitions);
+        }
         if(operatorDef.async === false){
           var code = FABRIC.loadResourceURL(operatorDef.srcFile, 'text/plain');
           code = FABRIC.preProcessCode(code, operatorDef.preProcessorDefinitions);
