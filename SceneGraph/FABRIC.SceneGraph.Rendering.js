5afeb22cd1518710a64ffbef457070195c44a1d0
Jerome Couture-Gagnon <jerome.couture-gagnon@fabric-engine.com>
2011-12-01 13:38:49 -0500

    Deferred render: mix with forward render using render targets instead of framebuffer depth blit

--- SceneGraph/FABRIC.SceneGraph.Rendering.js
+++ SceneGraph/FABRIC.SceneGraph.Rendering.js
@@ -165,17 +165,19 @@ FABRIC.SceneGraph.registerNodeType('BaseDeferredRenderer', {
   detailedDesc: '',
   parentNodeDesc: '',
   optionsDesc: {
-    colorBuffers: "array of render targets specifying 'nbChannels' (1 to 4) and 'name' for each target. Note that for a target name N, it is expected that the deferred shader will have a " + 
-                  "texture parameter name NTexture. Ex: [{nbChannels: 4, name: 'diffuse'}, {nbChannels: 3, name: 'normal'}], matching params diffuseTexture and normalTexture in deferred shader material." +
+    colorBuffers: "array of render targets specifying 'nbChannels' (1 to 4) and 'name' for each target, and optionally the internal OpenGL format (by default: 16F per channel). Note that for a target name N, it is expected that the deferred shader will have a " + 
+                  "texture parameter name NTexture. Ex: [{nbChannels: 4, name: 'diffuse'}, {nbChannels: 3, name: 'normal', internalFormat: FABRIC.SceneGraph.OpenGLConstants.GL_RGB16F}], matching params diffuseTexture and normalTexture in deferred shader material." +
                   "Note that your prePass shaders should then write to targets of the corresponding indices: glFragData[0]=diffuseColor; glFragData[1]=normal;",
     addDepth: "if depth should be attached",
     showDebug: "Display view of the render targets",
-    lightNode: "to be used by the Phong shading pass"
+    lightNode: "to be used by the Phong shading pass",
+    forwardRenderPass: "enables to mix with forward rendering"
   },
   factoryFn: function(options, scene) {
     scene.assignDefaults(options, {
       showDebug: false,
       addDepth: true,
+      forwardRenderPass: false,
       colorBuffers: []
       });
 
@@ -188,7 +190,7 @@ FABRIC.SceneGraph.registerNodeType('BaseDeferredRenderer', {
 
     scene.getSceneRedrawOpaqueObjectsEventHandler().appendChildEventHandler(redrawEventHandler);
 
-    var renderTargetRedrawEventHandler = deferredRenderNode.constructEventHandlerNode('RenderTargetRedraw');
+    var renderTargetRedrawEventHandler = deferredRenderNode.constructEventHandlerNode('DeferredPrePass');
     redrawEventHandler.appendChildEventHandler(renderTargetRedrawEventHandler);
 
     var channelToInternalFormat = [0, FABRIC.SceneGraph.OpenGLConstants.GL_R16F, FABRIC.SceneGraph.OpenGLConstants.GL_RG16F, FABRIC.SceneGraph.OpenGLConstants.GL_RGB16F, FABRIC.SceneGraph.OpenGLConstants.GL_RGBA16F];
@@ -205,15 +207,32 @@ FABRIC.SceneGraph.registerNodeType('BaseDeferredRenderer', {
       if(options.colorBuffers[i].name === undefined)
         throw ('Error: name not specified for render target ' + i);
 
+      var internalFormat = options.colorBuffers[i].internalFormat;
+      if(internalFormat === undefined)
+        internalFormat = channelToInternalFormat[nbChannels];
+
       oglRenderTargetTextureDescs.push(
           new FABRIC.RT.OGLRenderTargetTextureDesc (
               2, // COLOR_BUFFER
               new FABRIC.RT.OGLTexture2D (
-                channelToInternalFormat[nbChannels],
+                internalFormat,
                 channelToFormat[nbChannels],
                 FABRIC.SceneGraph.OpenGLConstants.GL_FLOAT)
             ));
       oglRenderTargetTextureNames.push(options.colorBuffers[i].name);
+    }
+
+    var forwardRenderBufferIndex = options.colorBuffers.length;
+    if(options.forwardRenderPass) {
+      oglRenderTargetTextureDescs.push(
+          new FABRIC.RT.OGLRenderTargetTextureDesc (
+              2, // COLOR_BUFFER
+              new FABRIC.RT.OGLTexture2D (
+                FABRIC.SceneGraph.OpenGLConstants.GL_RGBA8,
+                channelToFormat[4],
+                FABRIC.SceneGraph.OpenGLConstants.GL_FLOAT)
+            ));
+      oglRenderTargetTextureNames.push('forwardRender');
     }
 
     if(options.addDepth) {
@@ -237,17 +256,46 @@ FABRIC.SceneGraph.registerNodeType('BaseDeferredRenderer', {
     );
     redrawEventHandler.addMember('renderTarget', 'OGLRenderTarget', renderTarget);
 
-    renderTargetRedrawEventHandler.preDescendBindings.append(
-      scene.constructOperator({
-          operatorName: 'bindScreenRenderTarget',
-          srcFile: 'FABRIC_ROOT/SceneGraph/KL/renderTarget.kl',
-          entryFunctionName: 'bindScreenRenderTarget',
-          parameterLayout: [
-            'window.width',
-            'window.height',
-            'deferredDraw.renderTarget'
-          ]
-        }));
+    ///////////////////////////////
+    //Pre pass
+
+    if(!options.forwardRenderPass) {
+      renderTargetRedrawEventHandler.preDescendBindings.append(
+        scene.constructOperator({
+            operatorName: 'bindScreenRenderTarget',
+            srcFile: 'FABRIC_ROOT/SceneGraph/KL/renderTarget.kl',
+            entryFunctionName: 'bindScreenRenderTarget',
+            parameterLayout: [
+              'window.width',
+              'window.height',
+              'deferredDraw.renderTarget'
+            ]
+          }));
+    } else {
+      renderTargetRedrawEventHandler.preDescendBindings.append(
+        scene.constructOperator({
+            preProcessorDefinitions: {
+              FORWARD_RENDER_BUFFER_INDEX: forwardRenderBufferIndex
+            },
+            operatorName: 'bindPrePassRenderTargets',
+            srcCode:  'use OGLRenderTarget;\n' +
+                      'operator bindPrePassRenderTargets( io Integer width, io Integer height, io OGLRenderTarget renderTarget ) {\n' +
+                      '  renderTarget.prebind(width, height);\n' +
+                      '  renderTarget.bindFbo(true);\n' +
+                      '  Integer drawBufferIDs[];\n' +
+                      '  for(Size i=0; i < FORWARD_RENDER_BUFFER_INDEX; i++){\n' +
+                      '      drawBufferIDs.push(Integer(GL_COLOR_ATTACHMENT0 + drawBufferIDs.size()));\n' +
+                      '  }\n' +
+                      '  renderTarget.bindColorBuffers(drawBufferIDs);\n' +
+                      '}\n',
+            entryFunctionName: 'bindPrePassRenderTargets',
+            parameterLayout: [
+              'window.width',
+              'window.height',
+              'deferredDraw.renderTarget'
+            ]
+          }));
+    }
 
     renderTargetRedrawEventHandler.postDescendBindings.append(
       scene.constructOperator({
@@ -258,23 +306,6 @@ FABRIC.SceneGraph.registerNodeType('BaseDeferredRenderer', {
             'deferredDraw.renderTarget'
           ]
         }));
-
-    if(options.addDepth) {
-      renderTargetRedrawEventHandler.postDescendBindings.append(
-        scene.constructOperator({
-            operatorName: 'copyDepth',
-            srcCode: 'use OGLRenderTarget; operator copyDepth(io Integer width, io Integer height, io OGLRenderTarget renderTarget){\n'+
-                        'glBindFramebuffer(GL_READ_FRAMEBUFFER, renderTarget.fbo);\n' +
-                        'glBlitFramebuffer(0,0,width-1,height-1,0,0,width-1,height-1,GL_DEPTH_BUFFER_BIT, GL_NEAREST);\n'+
-                        'glBindFramebuffer(GL_READ_FRAMEBUFFER, GL_NONE);}',
-            entryFunctionName: 'copyDepth',
-            parameterLayout: [
-              'window.width',
-              'window.height',
-              'deferredDraw.renderTarget'
-            ]
-          }));
-    }
 
     redrawEventHandler.addMember('debugDrawToggle', 'Boolean', options.showDebug);
     redrawEventHandler.addMember('debugShaderProgID', 'Integer', 0);
@@ -310,8 +341,9 @@ FABRIC.SceneGraph.registerNodeType('BaseDeferredRenderer', {
           ]
     }));
     
-    //Shading pass
-    var shaderPassRedrawEventHandler = deferredRenderNode.constructEventHandlerNode('ShaderPass');
+    ///////////////////////////////
+    //Post (shading) pass
+    var shaderPassRedrawEventHandler = deferredRenderNode.constructEventHandlerNode('DeferredPostPass');
     redrawEventHandler.appendChildEventHandler(shaderPassRedrawEventHandler);
 
     shaderPassRedrawEventHandler.preDescendBindings.append(
@@ -396,6 +428,62 @@ FABRIC.SceneGraph.registerNodeType('BaseDeferredRenderer', {
       return scene.pub.constructNode(materialName, options);
     };
 
+    ///////////////////////////////
+    //Forward render mix pass
+
+    if(options.forwardRenderPass) {
+      var forwardRenderMixRedrawEventHandler = deferredRenderNode.constructEventHandlerNode('ForwardRenderMixPass');
+      redrawEventHandler.appendChildEventHandler(forwardRenderMixRedrawEventHandler);
+      forwardRenderMixRedrawEventHandler.addMember('shaderProgID', 'Integer', 0);
+
+      forwardRenderMixRedrawEventHandler.preDescendBindings.append(
+        scene.constructOperator({
+            preProcessorDefinitions: {
+              FORWARD_RENDER_BUFFER_INDEX: forwardRenderBufferIndex
+            },
+            operatorName: 'bindForwardRenderMixRenderTarget',
+            srcCode:  'use OGLRenderTarget;\n' +
+                      'operator bindForwardRenderMixRenderTarget( io OGLRenderTarget renderTarget ) {\n' +
+                      '  renderTarget.bindFbo(false);\n' +
+                      '  Integer drawBufferIDs[];\n' +
+                      '  drawBufferIDs.push(Integer(GL_COLOR_ATTACHMENT0 + FORWARD_RENDER_BUFFER_INDEX));\n' +
+                      '  glClearColor(0.0, 0.0, 0.0, 0.0);\n' +
+                      '  renderTarget.bindColorBuffers(drawBufferIDs);\n' +
+                      '}\n',
+            entryFunctionName: 'bindForwardRenderMixRenderTarget',
+            parameterLayout: [
+              'deferredDraw.renderTarget'
+            ]
+          }));
+
+      forwardRenderMixRedrawEventHandler.postDescendBindings.append(
+        scene.constructOperator({
+            preProcessorDefinitions: {
+              FORWARD_RENDER_BUFFER_INDEX: forwardRenderBufferIndex
+            },
+            operatorName: 'unbindAndDrawForwardRenderTarget',
+            srcCode:  'use OGLRenderTarget; use OGLTexture2D;\n' +
+                      'operator unbindAndDrawForwardRenderTarget( io OGLRenderTarget renderTarget, io Integer shaderProgramID ) {\n' +
+                      '  renderTarget.unbind();\n' +
+                      '  glPushAttrib(GL_ENABLE_BIT);\n' +
+                      '  glEnable(GL_BLEND);\n' +
+                      '  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);\n' +
+                      '  renderTarget.textures[FORWARD_RENDER_BUFFER_INDEX].texture.bind(0);\n' +
+                      '  drawTexture(0, shaderProgramID, Vec2(-1.0,1.0), Vec2(1.0,-1.0), false);\n' +
+                      '  glPopAttrib();}',
+            entryFunctionName: 'unbindAndDrawForwardRenderTarget',
+            parameterLayout: [
+              'deferredDraw.renderTarget',
+              'self.shaderProgID'
+              ]
+      }));
+
+      deferredRenderNode.pub.addForwardRenderMaterial = function(materialName, options) {
+        options.parentEventHandler = forwardRenderMixRedrawEventHandler;
+        return scene.pub.constructNode(materialName, options);
+      };
+    }
+
     return deferredRenderNode;
 }});
 
@@ -420,7 +508,7 @@ FABRIC.SceneGraph.registerNodeType('PhongDeferredRenderer', {
       definedDeferredPhongMaterials = true;
     }
 
-    options.colorBuffers = [{name: 'positions', nbChannels: 3}, {name: 'normals', nbChannels: 3}, {name: 'diffuseAndSpecularFactor', nbChannels: 4}];
+    options.colorBuffers = [{name: 'positions', nbChannels: 3}, {name: 'normals', nbChannels: 3}, {name: 'diffuseAndSpecularFactor', nbChannels: 4, internalFormat: FABRIC.SceneGraph.OpenGLConstants.GL_RGBA8}];
     options.addDepth = true;
 
     var deferredRenderNode = scene.constructNode('BaseDeferredRenderer', options);
