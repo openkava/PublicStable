859643eb849ea712c24365d98770a32645f9148f
Peter Zion <peter.zion@gmail.com>
2011-12-12 17:16:00 -0500

    Added MR::ConstArrayProducer

---
+++ Native/Core/MR/ConstArrayProducer.h
@@ -0,0 +1,68 @@
+/*
+ *  Copyright 2010-2011 Fabric Technologies Inc. All rights reserved.
+ */
+ 
+#ifndef _FABRIC_MR_CONST_ARRAY_PRODUCER_H
+#define _FABRIC_MR_CONST_ARRAY_PRODUCER_H
+
+#include <Fabric/Core/MR/ArrayProducer.h>
+
+#include <vector>
+#include <stdint.h>
+
+namespace Fabric
+{
+  namespace JSON
+  {
+    class Array;
+  };
+  
+  namespace RT
+  {
+    class Manager;
+    class VariableArrayDesc;
+  };
+  
+  namespace MR
+  {
+    class ConstArrayProducer : public ArrayProducer
+    {
+      FABRIC_GC_OBJECT_GET_CLASS_DECL()
+      
+      // Virtual functions: ArrayProducer
+    
+    public:
+    
+      static RC::Handle<ConstArrayProducer> Create(
+        GC::Container *container,
+        RC::ConstHandle<RT::Manager> const &rtManager,
+        RC::ConstHandle<RT::Desc> const &elementDesc,
+        RC::ConstHandle<JSON::Array> const &jsonArray
+        );
+    
+      virtual size_t count() const;
+      virtual void produce( size_t index, void *data ) const;
+            
+    protected:
+    
+      ConstArrayProducer(
+        GC::Object::Class const *myClass,
+        GC::Container *container,
+        RC::ConstHandle<RT::Manager> const &rtManager,
+        RC::ConstHandle<RT::Desc> const &elementDesc,
+        RC::ConstHandle<JSON::Array> const &jsonArray
+        );
+      ~ConstArrayProducer();
+    
+      virtual char const *getKind() const;
+      virtual void toJSONImpl( Util::JSONObjectGenerator &jog ) const;
+    
+    private:
+    
+      RC::ConstHandle<RT::VariableArrayDesc> m_variableArrayDesc;
+      std::vector<uint8_t> m_data;
+    };
+  };
+};
+
+#endif //_FABRIC_MR_CONST_ARRAY_PRODUCER_H
