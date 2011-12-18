92f93d3d123ef5e5deb645670e7dbd1a3999000a
Peter Zion <peter.zion@gmail.com>
2011-12-18 15:43:00 -0500

    MR::ReduceOperator: support shared data

--- Native/Core/KLC/ReduceOperator.h
+++ Native/Core/KLC/ReduceOperator.h
@@ -21,11 +21,42 @@ namespace Fabric
   
   namespace KLC
   {
-#define FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL( typeName ) \
-      void call2_##typeName( void const *inputData, void *outputData ); \
-      void call3_##typeName( void const *inputData, void *outputData, size_t index ); \
-      void call4_##typeName( void const *inputData, void *outputData, size_t index, size_t count ); \
-  
+#define FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN_STN( inputTypeName, sharedTypeName ) \
+      void call5##inputTypeName##sharedTypeName( void const *inputData, void *outputData, size_t index, size_t count, void const *sharedData ); \
+      
+#define FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN( inputTypeName ) \
+      void call2##inputTypeName( void const *inputData, void *outputData ); \
+      void call3##inputTypeName( void const *inputData, void *outputData, size_t index ); \
+      void call4##inputTypeName( void const *inputData, void *outputData, size_t index, size_t count ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN_STN( inputTypeName, Boolean ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN_STN( inputTypeName, UInt8 ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN_STN( inputTypeName, SInt8 ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN_STN( inputTypeName, UInt16 ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN_STN( inputTypeName, SInt16 ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN_STN( inputTypeName, UInt32 ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN_STN( inputTypeName, SInt32 ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN_STN( inputTypeName, UInt64 ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN_STN( inputTypeName, SInt64 ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN_STN( inputTypeName, Float32 ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN_STN( inputTypeName, Float64 ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN_STN( inputTypeName, String ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN_STN( inputTypeName, Default ); \
+      
+#define FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL() \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN( Boolean ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN( UInt8 ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN( SInt8 ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN( UInt16 ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN( SInt16 ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN( UInt32 ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN( SInt32 ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN( UInt64 ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN( SInt64 ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN( Float32 ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN( Float64 ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN( String ); \
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL_ITN( Default ); \
+
     class ReduceOperator : public Operator
     {
       FABRIC_GC_OBJECT_CLASS_DECL()
@@ -42,6 +73,7 @@ namespace Fabric
         
       RC::ConstHandle<RT::Desc> getInputDesc() const;
       RC::ConstHandle<RT::Desc> getOutputDesc() const;
+      RC::ConstHandle<RT::Desc> getSharedDesc() const;
       
       bool takesIndex() const
       {
@@ -50,6 +82,10 @@ namespace Fabric
       bool takesCount() const
       {
         return m_call4 != 0;
+      }
+      bool takesSharedData() const
+      {
+        return m_call5 != 0;
       }
       
       void call(
@@ -67,6 +103,13 @@ namespace Fabric
         size_t index,
         size_t count
         ) const;
+      void call(
+        void const *inputData,
+        void *outputData,
+        size_t index,
+        size_t count,
+        void const *sharedData
+        ) const;
       
     protected:
     
@@ -79,26 +122,16 @@ namespace Fabric
     
     private:
     
-      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL( Boolean )
-      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL( UInt8 )
-      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL( SInt8 )
-      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL( UInt16 )
-      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL( SInt16 )
-      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL( UInt32 )
-      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL( SInt32 )
-      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL( UInt64 )
-      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL( SInt64 )
-      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL( Float32 )
-      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL( Float64 )
-      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL( String )
-      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL( Default )
+      FABRIC_KLC_REDUCE_OPERATOR_CALL_DECL()
     
       RC::ConstHandle<CG::Adapter> m_inputAdapter;
       RC::ConstHandle<CG::Adapter> m_outputAdapter;
+      RC::ConstHandle<CG::Adapter> m_sharedAdapter;
       
       void (ReduceOperator::*m_call2)( void const *inputData, void *outputData );
       void (ReduceOperator::*m_call3)( void const *inputData, void *outputData, size_t index );
       void (ReduceOperator::*m_call4)( void const *inputData, void *outputData, size_t index, size_t count );
+      void (ReduceOperator::*m_call5)( void const *inputData, void *outputData, size_t index, size_t count, void const *sharedData );
     };
   }
 }
