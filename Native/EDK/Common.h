2afc84b47756ac7e0d712d1672d861181777444d
Jerome Couture-Gagnon <jerome.couture-gagnon@fabric-engine.com>
2011-12-23 13:44:57 -0500

    Initial implementation of FabricFileHandle native type (leaking; to be fixed later).

--- Native/EDK/Common.h
+++ Native/EDK/Common.h
@@ -16,12 +16,22 @@ namespace Fabric
         , m_realloc(NULL)
         , m_free(NULL)
         , m_throwException(NULL)
+        , m_fabricFileHandleCopy(NULL)
+        , m_fabricFileHandleDelete(NULL)
+        , m_fabricFileHandleSetFromPath(NULL)
+        , m_fabricFileHandleGetFullPath(NULL)
+        , m_fabricFileHandleHasReadWriteAccess(NULL)
       {}
 
       void *( *m_malloc )( size_t );
       void *( *m_realloc )( void *, size_t );
       void ( *m_free )( void * );
       void (*m_throwException)( size_t length, char const *data );
+      void ( *m_fabricFileHandleCopy )( void **, void const * );
+      void ( *m_fabricFileHandleDelete )( void ** );
+      bool ( *m_fabricFileHandleSetFromPath )( void **, char const *, size_t, bool );
+      char const *( *m_fabricFileHandleGetFullPath )( void const * );
+      bool ( *m_fabricFileHandleHasReadWriteAccess )( void const * );
     };
   };
 };
