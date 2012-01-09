19b9f3e12d8e8d6e6a14c49af375feb02c76a6cf
Helge Mathee <helge.mathee@fabric-engine.com>
2012-01-09 14:35:04 +0100

    alembic import broken due to string changes

--- Native/Exts/Builtin/ALEMBIC/alembic.cpp
+++ Native/Exts/Builtin/ALEMBIC/alembic.cpp
@@ -232,33 +232,34 @@ FABRIC_EXT_EXPORT void FabricALEMBICGetIdentifiers(
     offset = 0;
     for(size_t i=1;i<iObjects.size();i++)
     {
-      identifiers[offset] = iObjects[i].getFullName().c_str();
-      identifiers[offset] += "|";
+      std::string identifier = iObjects[i].getFullName().c_str();
+      identifier += "|";
       int numSamples = 1;
       const Alembic::Abc::MetaData &md = iObjects[i].getMetaData();
       if(Alembic::AbcGeom::IXform::matches(md)) {
-        identifiers[offset] += "Xform|";
+        identifier += "Xform|";
         numSamples = Alembic::AbcGeom::IXform(iObjects[i],Alembic::Abc::kWrapExisting).getSchema().getNumSamples();
       } else if(Alembic::AbcGeom::IPolyMesh::matches(md)) {
-        identifiers[offset] += "PolyMesh|";
+        identifier += "PolyMesh|";
         numSamples = Alembic::AbcGeom::IPolyMesh(iObjects[i],Alembic::Abc::kWrapExisting).getSchema().getNumSamples();
       } else if(Alembic::AbcGeom::ICurves::matches(md)) {
-        identifiers[offset] += "Curves|";
+        identifier += "Curves|";
         numSamples = Alembic::AbcGeom::ICurves(iObjects[i],Alembic::Abc::kWrapExisting).getSchema().getNumSamples();
       } else if(Alembic::AbcGeom::INuPatch::matches(md)) {
-        identifiers[offset] += "NuPatch|";
+        identifier += "NuPatch|";
         numSamples = Alembic::AbcGeom::INuPatch(iObjects[i],Alembic::Abc::kWrapExisting).getSchema().getNumSamples();
       } else if(Alembic::AbcGeom::IPoints::matches(md)) {
-        identifiers[offset] += "Points|";
+        identifier += "Points|";
         numSamples = Alembic::AbcGeom::IPoints(iObjects[i],Alembic::Abc::kWrapExisting).getSchema().getNumSamples();
       } else if(Alembic::AbcGeom::ISubD::matches(md)) {
-        identifiers[offset] += "SubD|";
+        identifier += "SubD|";
         numSamples = Alembic::AbcGeom::ISubD(iObjects[i],Alembic::Abc::kWrapExisting).getSchema().getNumSamples();
       } else if(Alembic::AbcGeom::ICamera::matches(md)) {
-        identifiers[offset] += "Camera|";
+        identifier += "Camera|";
         numSamples = Alembic::AbcGeom::ICamera(iObjects[i],Alembic::Abc::kWrapExisting).getSchema().getNumSamples();
       }
-      identifiers[offset] += boost::lexical_cast<std::string>(numSamples).c_str();
+      identifier += boost::lexical_cast<std::string>(numSamples).c_str();
+      identifiers[offset] = identifier.c_str();
       offset++;
     }
   }
