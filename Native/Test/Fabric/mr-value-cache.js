4a003a797653140a8dc7e99bbb0d36a606e46b7b
Andrew MacPherson <andrew.macp@gmail.com>
2012-01-20 11:28:37 -0500

    Add javascript unit tests.

---
+++ Native/Test/Fabric/mr-value-cache.js
@@ -0,0 +1,22 @@
+FC = require('Fabric').createClient();
+
+var gen = 
+  FC.MR.createValueCache(
+    FC.MR.createValueGenerator(
+      FC.KLC.createValueGeneratorOperator(
+        "xfo.kl", "operator xfo(io Integer value) { report 'Running generator'; value = 121; }", "xfo"
+        )
+      )
+    );
+
+console.log("Should run generator");
+console.log(gen.produce());
+
+console.log("Should not run generator (cached)");
+console.log(gen.produce());
+
+gen.flush();
+console.log("Should run generator");
+console.log(gen.produce());
+
+FC.close();
