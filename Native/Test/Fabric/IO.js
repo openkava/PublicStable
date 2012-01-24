7a1cdf4e51bae116289838eeb55a36edb9c779bc
Jerome Couture-Gagnon <jerome.couture-gagnon@fabric-engine.com>
2012-01-24 00:30:41 -0500

    FabricHandle: untested unit tests (node module doesn't load for some reason)

--- Native/Test/Fabric/IO.js
+++ Native/Test/Fabric/IO.js
@@ -4,14 +4,14 @@ node = FABRIC.DependencyGraph.createNode("node");
 node.addMember("resource", "FabricResource");
 node.addMember("resource2", "FabricResource");
 
-var path = FABRIC.IO.queryUserFileAndFolderHandle(FABRIC.IO.forOpenWithWriteAccess, "dummy title", "txt", "testfile");
+var fileAndFolderHandles = FABRIC.IO.queryUserFileAndFolderHandle(FABRIC.IO.forOpenWithWriteAccess, "dummy title", "txt", "testfile");
 console.log(path.fileName);
 
-var path2 = FABRIC.IO.queryUserFileAndFolderHandle(FABRIC.IO.forOpen, "dummy title", "txt", "testfile");
+var fileAndFolderHandles = FABRIC.IO.queryUserFileAndFolderHandle(FABRIC.IO.forOpen, "dummy title", "txt", "testfile");
 
 try
 {
-  FABRIC.IO.putTextFile("dummy", "pathShouldNotBeString");
+  FABRIC.IO.putTextFileContent("dummy", "badHandle");
 }
 catch(e)
 {
@@ -19,73 +19,39 @@ catch(e)
 }
 
 try {
-  FABRIC.IO.putTextFile("dummy", path2);
+  FABRIC.IO.putTextFile("dummy", fileAndFolderHandles.file);
 }
 catch (e) {
   console.log("ErrorCantWriteIfOpenMode");//Can't console.log 'e' since it contains random bytes
 }
 
-var path3 = {
-  folderHandle: "badHandle",
-  fileName: "dummy.txt"
-  };
+var path4 = FABRIC.IO.buildFileHandleFromRelativePath(fileAndFolderHandles.folder + '/SubDir/testfile2.txt');
 
 try {
-  var badPath = FABRIC.IO.getTextFile(path3);
+  var badPath = FABRIC.IO.getTextFileContent(path4);
 }
 catch (e) {
-  console.log(e);
-}
+  console.log(path4);
+  }
 
-var path4 = {
-  folderHandle: path.folderHandle,
-  subFolders: ["SubDir"],
-  fileName: "testfile2.txt"
-  };
-
-try {
-  var badPath = FABRIC.IO.getTextFile(path4);
-}
-catch (e) {
-  console.log("DirNotFound");//Can't console.log 'e' since it contains random bytes
-}
-
-FABRIC.IO.putTextFile("FileBody", path4);
-var body = FABRIC.IO.getTextFile(path4);
+FABRIC.IO.putTextFileContent(path4,"FileBody");
+var body = FABRIC.IO.getTextFileContent(path4);
 console.log(body);
 
-FABRIC.IO.putUserTextFile("FileBody2", "dummyTitle", "txt", "default");
-var body2 = FABRIC.IO.getUserTextFile("dummyTitle.txt", "*"); //Extension::IOManager hardcoded to get file "default.txt"
-console.log(body2);
+FABRIC.IO.putTextFileContent(path4," append text",true);
+var body = FABRIC.IO.getTextFileContent(path4);
+console.log(body);
 
-node.getResourceFromUserFile("resource", "", "txt"); //Extension::IOManager hardcoded to get file "default.txt"
+FABRIC.IO.getFileInfo(path4);
+var fileInfo = FABRIC.IO.getTextFileContent(path4);
+console.log(fileInfo);
+
+node.setData('url',0,path4);
 console.log(JSON.stringify(node.getData("resource")));
-node.putResourceToUserFile("resource", "", "txt", "testfile3");
-var path5 = {
-  folderHandle: path.folderHandle,
-  fileName: "testfile3.txt"
-  };
+var path5 = FABRIC.IO.buildFileHandleFromRelativePath(fileAndFolderHandles.folder + '/testfile3.txt');
+node.putResourceToFile(path5);
 
-var body3 = FABRIC.IO.getTextFile(path5);
+var body3 = FABRIC.IO.getTextFileContent(path5);
 console.log(body3);
-
-var path6 = {
-  folderHandle: path.folderHandle,
-  subFolders: ["SubDir"],
-  fileName: "testfile4.txt"
-  };
-
-FABRIC.IO.putTextFile("FileBody3", path6);
-node.getResourceFromFile("resource", path6);
-console.log(JSON.stringify(node.getData("resource")));
-
-var path7 = {
-  folderHandle: path.folderHandle,
-  subFolders: ["SubDir"],
-  fileName: "testfile5.txt"
-  };
-node.putResourceToFile(path7,"resource");
-var body4 = FABRIC.IO.getTextFile(path7);
-console.log(body4);
 
 FABRIC.close();
