66d3400a19466daa28448aee5a8efc82ee0f8425
Andrew MacPherson <andrew.macp@gmail.com>
2012-01-24 15:25:49 -0500

    Bunch of changes, adding more MR functionality. Currently broken but checking in since lots of changes have been made.

--- Native/Clients/Python/__init__.py
+++ Native/Clients/Python/__init__.py
@@ -1,41 +1,57 @@
 import os
-import ctypes
 import json
+import ctypes
 import collections
 
 # FIXME fix object hierarchy, static variables, visibility
 
 # FIXME Windows
-fabric = ctypes.CDLL( os.path.dirname( __file__ ) + '/libFabricPython.so' )
+if os.name == 'posix':
+  fabric = ctypes.CDLL( os.path.dirname( __file__ ) + '/libFabricPython.so' )
+else:
+  raise Exception('not implemented for Windows yet!')
 
 def createClient():
-  return CLIENT( fabric )
- 
-class CLIENT(object):
+  return INTERFACE( fabric )
+
+# this is the interface object that gets returned to the user
+class INTERFACE( object ):
   def __init__( self, fabric ):
-    self.__client_ptr = fabric.createClient()
+    self.__client = CLIENT( fabric )
+    self.KLC = KLC( self.__client )
+    self.MR = MR( self.__client )
+    self.RT = RT( self.__client )
+
+class CLIENT( object ):
+  def __init__( self, fabric ):
+    self.__fabric_client = fabric.createClient()
     self.__queuedCommands = [];
     self.__queuedUnwinds = [];
     self.__queuedCallbacks = [];
-
     self.GC = GC( self )
-    self.MR = MR( self )
 
   def __del__( self ):
-    self.__close()
+    self.close()
 
   def __jsonExec( self, data, length ):
     result = ctypes.c_char_p()
     rlength = ctypes.c_size_t()
 
-    fabric.jsonExec( self.__client_ptr, data, length, ctypes.pointer( result ), ctypes.pointer( rlength ) )
+    fabric.jsonExec(
+      self.__fabric_client,
+      data,
+      length,
+      ctypes.pointer( result ),
+      ctypes.pointer( rlength )
+    )
 
+    # FIXME we're ignoring rlength for now
     return result
 
   def close( self ):
-    if self.__client_ptr != None:
-      fabric.close( self.__client_ptr )
-      self.__client_ptr = None
+    if self.__fabric_client != None:
+      fabric.close( self.__fabric_client )
+      self.__fabric_client = None
 
   def queueCommand( self, dst, cmd, arg = None, unwind = None, callback = None ):
     command = { 'dst': dst, 'cmd': cmd }
@@ -73,24 +89,169 @@ class CLIENT(object):
 
     fabric.freeString( jsonEncodedResults )
 
-#  def test( self ):
-#    self.__jsonExec( '[{"dst":["MR"],"cmd":"createConstValue","arg":{"id":"GC_0","valueType":"Size","data":10000}},{"dst":["MR","GC_0"],"cmd":"produce","arg":null}]', 142 )
-
 class MR( object ):
   def __init__( self, client ):
     self.__client = client
 
+  def createConstArray( self, elementType, data = None ):
+    valueArray = ARRAYPRODUCER( self.__client )
+
+    arg = { 'id': valueArray.getID() }
+    if type( elementType ) is str:
+      arg[ 'elementType' ] = elementType
+      arg[ 'data' ] = data
+    elif type ( elementType ) is dict:
+      inputArg = elementType
+      arg[ 'elementType' ] = inputArg[ 'elementType' ]
+      if 'data' in inputArg:
+        arg[ 'data' ] = inputArg[ 'data' ]
+      if 'jsonData' in inputArg:
+        arg[ 'jsonData' ] = inputArg[ 'jsonData' ]
+    else:
+      raise Exception( "createConstArray: first argument must be str or dict" )
+
+    self.__client.queueCommand( ['MR'], 'createConstArray', arg, valueArray.unwind )
+    return valueArray
+
   def createConstValue( self, valueType, data ):
     value = VALUEPRODUCER( self.__client )
-    arg = { 'id': value.getID(), 'valueType': valueType, 'data': data }
+    arg = {
+      'id': value.getID(),
+      'valueType': valueType,
+      'data': data
+    }
     self.__client.queueCommand( ['MR'], 'createConstValue', arg, value.unwind )
     return value
 
   def createValueCache( self, input ):
-    value = VALUEPRODUCER( self.__client )
-    arg = { 'id': value.getID(), 'inputID': input.getID() }
-    self.__client.queueCommand( ['MR'], 'createValueCache', arg, value.unwind )
-    return value
+    return self.__createMRCommand( VALUEPRODUCER( self.__client ), 'createValueCache', input, None, None )
+
+  def createValueGenerator( self, operator ):
+    return self.__createMRCommand( VALUEPRODUCER( self.__client ), 'createValueGenerator', None, operator, None )
+
+  def createValueMap( self, input, operator, shared = None ):
+    return self.__createMRCommand( VALUEPRODUCER( self.__client ), 'createValueMap', input, operator, shared )
+
+  def createValueTransform( self, input, operator, shared = None ):
+    return self.__createMRCommand( VALUEPRODUCER( self.__client ), 'createValueTransform', input, operator, shared )
+
+  def createArrayCache( self, input ):
+    return self.__createMRCommand( ARRAYPRODUCER( self.__client ), 'createArrayCache', input, None, None )
+
+  def createArrayGenerator( self, count, operator, shared = None ):
+    obj = ARRAYPRODUCER( self.__client )
+    arg = {
+      'id': obj.getID(),
+      'countID': count.getID(),
+      'operatorID': operator.getID()
+    }
+    if ( shared ):
+      arg[ 'sharedID' ] = shared.getID()
+
+    self.__client.queueCommand( ['MR'], 'createArrayGenerator', arg, obj.unwind )
+    return obj
+
+  def createArrayMap( self, input, operator, shared = None ):
+    return self.__createMRCommand( ARRAYPRODUCER( self.__client ), 'createArrayMap', input, operator, shared )
+
+  def createArrayTransform( self, input, operator, shared = None ):
+    return self.__createMRCommand( ARRAYPRODUCER( self.__client ), 'createArrayTransform', input, operator, shared )
+
+  def createReduce( self, inputArrayProducer, reduceOperator, sharedValueProducer = None ):
+    reduce = VALUEPRODUCER( self.__client )
+    arg = {
+      'id': reduce.getID(),
+      'inputArrayProducerID': inputArrayProducer.getID(),
+      'reduceOperatorID': reduceOperator.getID()
+    }
+    if ( sharedValueProducer ):
+      arg[ 'sharedValueProducerID' ] = sharedValueProducer.getID()
+
+    self.__client.queueCommand( ['MR'], 'createReduce', arg, reduce.unwind )
+    return reduce
+
+  def __createMRCommand( self, obj, cmd, input, operator, shared ):
+    arg = {
+      'id': obj.getID()
+    }
+    if ( input ):
+      arg[ 'inputID' ] = input.getID()
+    if ( operator ):
+      arg[ 'operatorID' ] = operator.getID()
+    if ( shared ):
+      arg[ 'sharedID' ] = shared.getID()
+
+    self.__client.queueCommand( ['MR'], cmd, arg, obj.unwind )
+    return obj
+
+class KLC( object ):
+  def __init__( self, client ):
+    self.__client = client
+
+  def __createOperator( self, sourceName, sourceCode, operatorName, cmd ):
+    operator = OPERATOR( self.__client )
+    arg = {
+      'id': operator.getID(),
+      'sourceName': sourceName,
+      'sourceCode': sourceCode,
+      'operatorName': operatorName
+    }
+
+    self.__client.queueCommand( ['KLC'], cmd, arg, operator.unwind )
+    return operator
+
+  def createReduceOperator( self, sourceName, sourceCode, operatorName ):
+    return self.__createOperator( sourceName, sourceCode, operatorName, 'createReduceOperator' )
+
+  def createValueGeneratorOperator( self, sourceName, sourceCode, operatorName ):
+    return self.__createOperator( sourceName, sourceCode, operatorName, 'createValueGeneratorOperator' )
+
+  def createValueMapOperator( self, sourceName, sourceCode, operatorName ):
+    return self.__createOperator( sourceName, sourceCode, operatorName, 'createValueMapOperator' )
+
+  def createValueTransformOperator( self, sourceName, sourceCode, operatorName ):
+    return self.__createOperator( sourceName, sourceCode, operatorName, 'createValueTransformOperator' )
+
+  def createArrayGeneratorOperator( self, sourceName, sourceCode, operatorName ):
+    return self.__createOperator( sourceName, sourceCode, operatorName, 'createArrayGeneratorOperator' )
+
+  def createArrayMapOperator( self, sourceName, sourceCode, operatorName ):
+    return self.__createOperator( sourceName, sourceCode, operatorName, 'createArrayMapOperator' )
+
+  def createArrayTransformOperator( self, sourceName, sourceCode, operatorName ):
+    return self.__createOperator( sourceName, sourceCode, operatorName, 'createArrayTransformOperator' )
+
+class RT( object ):
+  def __init__( self, client ):
+    self.__client = client
+
+  #def getRegisteredTypes( self ):
+
+  def registerType( self, name, desc ):
+    members = [ ]
+    for descMemberName in desc[ 'members' ]:
+      member = {
+        'name': descMemberName,
+        'type': desc[ 'members' ][ descMemberName ]
+      }
+      members.append( member )
+
+    defaultValue = None
+
+    arg = {
+      'name': name,
+      'members': members,
+      'defaultValue': defaultValue
+    }
+    if ( 'klBindings' in desc ):
+      arg[ 'klBindings' ] = desc[ 'klBindings' ]
+
+    def __queueCommandUnwind():
+      print 'unwind'
+      # FIXME unwind
+      #del RT.prototypes[ name ]
+
+    self.__client.queueCommand( ['RT'], 'registerType', arg, __queueCommandUnwind )
 
 class GCOBJECT( object ):
   def __init__( self, client, namespace ):
@@ -109,7 +270,7 @@ class GCOBJECT( object ):
     self._client.GC.disposeObject( self )
     self.__id = None
  
-  def _queueCommand( self, cmd, arg, unwind, callback ):
+  def _queueCommand( self, cmd, arg = None, unwind = None, callback = None ):
     if self.__id == None:
       raise Exception( "GC object has already been disposed" )
     self._client.queueCommand( [self.__namespace, self.__id], cmd, arg, unwind, callback )
@@ -129,21 +290,60 @@ class GCOBJECT( object ):
   def getID( self ):
     return self.__id
 
+  def setID( self, id ):
+    self.__id = id
+
 class PRODUCER( GCOBJECT ):
   def __init__( self, client ):
     super( PRODUCER, self ).__init__( client, 'MR' )
 
-  def __toJSON( self, result ):
-    self.__json = result
+  # FIXME this doesn't make sense, should this be disposing?
+  def unwind( self ):
+    self.setID( None )
 
   def toJSON( self ):
     # dictionary hack to simulate Python 3.x nonlocal
     json = { '_': None }
     def __toJSON( result ):
-      json[ '_' ] = data
+      json[ '_' ] = result
 
     self._queueCommand( 'toJSON', None, None, __toJSON )
     return json[ '_' ]
+
+class ARRAYPRODUCER( PRODUCER ):
+  def __init__( self, client ):
+    super( ARRAYPRODUCER, self ).__init__( client )
+
+  def getCount( self ):
+    # dictionary hack to simulate Python 3.x nonlocal
+    count = { '_': None }
+    def __getCount( result ):
+      count[ '_' ] = result
+
+    self._queueCommand( 'getCount', None, None, __getCount )
+    return count[ '_' ]
+
+  def produce( self, index, count ):
+    arg = { }
+    if ( index ):
+      if ( count ):
+        arg[ 'count' ] = count
+      arg[ 'index' ] = index
+
+    # dictionary hack to simulate Python 3.x nonlocal
+    result = { '_': None }
+    def __produce( data ):
+      result[ '_' ] = data
+
+    self._queueCommand( 'produce', arg, None, __produce )
+    self._client.executeQueuedCommands()
+    return result[ '_' ]
+
+  def flush( self ):
+    self._queueCommand( 'flush' )
+
+  # FIXME implement produceAsync
+  #def produceAsync( self, b0
 
 class VALUEPRODUCER( PRODUCER ):
   def __init__( self, client ):
@@ -157,7 +357,6 @@ class VALUEPRODUCER( PRODUCER ):
 
     self._queueCommand( 'produce', None, None, __produce )
     self._client.executeQueuedCommands()
-
     return result[ '_' ]
 
   def produceAsync( self, callback ):
@@ -167,9 +366,19 @@ class VALUEPRODUCER( PRODUCER ):
   def flush( self ):
     self._queueCommand( 'flush' )
 
-  # FIXME this doesn't make sense, should this be disposing?
-  def unwind( self ):
-    self.__id = None
+# FIXME doesn't really derive from PRODUCER but shared an identical toJSON method
+class OPERATOR( PRODUCER ):
+  def __init__( self, client ):
+    super( OPERATOR, self ).__init__( client )
+
+  def getDiagnostics( self ):
+    # dictionary hack to simulate Python 3.x nonlocal
+    diagnostics = { '_': None }
+    def __getDiagnostics( result ):
+      diagnostics[ '_' ] = result
+
+    self._queueCommand( 'getDiagnostics', None, None, __getDiagnostics )
+    return diagnostics[ '_' ]
 
 class GC( object ):
   def __init__( self, client ):
