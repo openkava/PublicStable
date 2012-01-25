b6cc8ed1d5e42a8ab9eb7a7b3a29c7a53aa39459
Helge Mathee <helge.mathee@fabric-engine.com>
2012-01-25 09:36:18 +0100

    Video Extension: Encoding basics

--- Native/Exts/Builtin/VIDEO/video.cpp
+++ Native/Exts/Builtin/VIDEO/video.cpp
@@ -22,6 +22,26 @@ extern "C" {
 #include <libswscale/swscale.h>
 }
 
+static AVFrame *alloc_picture(enum PixelFormat pix_fmt, int width, int height)
+{
+  AVFrame *picture;
+  uint8_t *picture_buf;
+  int size;
+  
+  picture = avcodec_alloc_frame();
+  if (!picture)
+    return NULL;
+  size = avpicture_get_size(pix_fmt, width, height);
+  picture_buf = (uint8_t*)av_malloc(size);
+  if (!picture_buf) {
+    av_free(picture);
+    return NULL;
+  }
+  avpicture_fill((AVPicture *)picture, picture_buf,
+  pix_fmt, width, height);
+  return picture;
+}
+
 class videoStream;
 
 const float cFrameSkipTolerance = 0.005f;
@@ -43,6 +63,7 @@ private:
   bool mReadOnly;
   AVFormatContext * mFormatCtx;
   AVCodecContext * mCodecCtx;
+  AVStream * mVideoStream;
   int mVideoStreamID;
   AVCodec * mCodec;
   AVFrame * mFrameYUV;
@@ -50,6 +71,8 @@ private:
   uint8_t * mBuffer;
   SwsContext * mConvertCtx;
   videoHandle * mHandle;
+  uint8_t * mVideoBuffer;
+  size_t mVideoBufferSize;
 
 public:
     
@@ -71,28 +94,53 @@ public:
     mFrameRGB = NULL;
     mBuffer = NULL;
     mConvertCtx = NULL;
+    mVideoBuffer = NULL;
+    mVideoBufferSize = 0;
   }
   
   ~videoStream()
   {
-    av_free(mBuffer);
-    av_free(mFrameRGB);
-    av_free(mFrameYUV);
-    avcodec_close(mCodecCtx);
-    av_close_input_file(mFormatCtx);
+    if(mReadOnly)
+    {
+      av_free(mBuffer);
+      av_free(mFrameRGB);
+      av_free(mFrameYUV);
+      avcodec_close(mCodecCtx);
+      av_close_input_file(mFormatCtx);
+    }
+    else
+    {
+      av_write_trailer(mFormatCtx);
+
+      avcodec_close(mVideoStream->codec);
+      av_free(mFrameRGB->data[0]);
+      av_free(mFrameRGB);
+      av_free(mVideoBuffer);
+
+      /* free the streams */
+      for(size_t i = 0; i < mFormatCtx->nb_streams; i++) {
+        av_freep(&mFormatCtx->streams[i]->codec);
+        av_freep(&mFormatCtx->streams[i]);
+      }
+      
+      if (!(mFormatCtx->oformat->flags & AVFMT_NOFILE)) {
+        /* close the output file */
+        url_fclose(mFormatCtx->pb);
+      }
+      
+      /* free the format ctx */
+      av_free(mFormatCtx);
+
+    }
   }
   
   KL::Size getWidth()
   {
-    if(!mReadOnly)
-      return 0;
     return mCodecCtx ? mCodecCtx->width : 0;
   }
   
   KL::Size getHeight()
   {
-    if(!mReadOnly)
-      return 0;
     return mCodecCtx ? mCodecCtx->height : 0;
   }
   
@@ -201,6 +249,95 @@ public:
       mHandle->duration = getDuration();
       mHandle->fps = getFPS();
     }
+    else
+    {
+      // init the video for writing
+      AVOutputFormat * fmt = av_guess_format("h264",NULL,NULL);
+      if(!fmt)
+      {
+        Fabric::EDK::throwException("Video Extension: h264 format not found!");
+        return false;
+      }
+      mFormatCtx = avformat_alloc_context();
+      mFormatCtx->oformat = fmt;
+      
+      // store the filename
+      snprintf(mFormatCtx->filename, sizeof(mFormatCtx->filename), "%s", filename.data());
+      
+      // create a video stream
+      mVideoStream = av_new_stream(mFormatCtx, 0);
+
+      mCodecCtx = mVideoStream->codec;
+      mCodecCtx->codec_id = fmt->video_codec;
+      mCodecCtx->codec_type = AVMEDIA_TYPE_VIDEO;
+
+      mCodecCtx->bit_rate = 400000;
+      mCodecCtx->width = width;
+      mCodecCtx->height = height;
+      /* time base: this is the fundamental unit of time (in seconds) in terms
+         of which frame timestamps are represented. for fixed-fps content,
+         timebase should be 1/framerate and timestamp increments should be
+         identically 1. */
+      mCodecCtx->time_base.den = 25;
+      mCodecCtx->time_base.num = 1;
+      mCodecCtx->gop_size = 12; /* emit one intra frame every twelve frames at most */
+      mCodecCtx->pix_fmt = PIX_FMT_RGB24;
+
+      // some formats want stream headers to be separate
+      if(fmt->flags & AVFMT_GLOBALHEADER)
+        mCodecCtx->flags |= CODEC_FLAG_GLOBAL_HEADER;      
+
+      /* set the output parameters (must be done even if no
+         parameters). */
+      if (av_set_parameters(mFormatCtx, NULL) < 0) {
+        Fabric::EDK::throwException("Video Extension: Invalid output format parameters!");
+        return false;
+      }
+      //av_dump_format(mCodecCtx, 0, filename.data(), 1);
+      
+      /* find the video encoder */
+      AVCodec * codec = avcodec_find_encoder(mCodecCtx->codec_id);
+      if (!codec) {
+        Fabric::EDK::throwException("Video Extension: Codec not found!");
+        return false;
+      }
+
+      /* open the codec */
+      if (avcodec_open(mCodecCtx, codec) < 0) {
+        Fabric::EDK::throwException("Video Extension: Could not open codec!");
+        return false;
+      }
+
+      mVideoBuffer = NULL;
+      if (!(mFormatCtx->oformat->flags & AVFMT_RAWPICTURE)) {
+        /* allocate output buffer */
+        /* XXX: API change will be done */
+        /* buffers passed into lav* can be allocated any way you prefer,
+        as long as they're aligned enough for the architecture, and
+        they're freed appropriately (such as using av_free for buffers
+        allocated with av_malloc) */
+        mVideoBufferSize = 200000;
+        mVideoBuffer = (uint8_t*)av_malloc(mVideoBufferSize);
+      }
+
+      /* allocate the encoded raw picture */
+      mFrameRGB = alloc_picture(mCodecCtx->pix_fmt, mCodecCtx->width, mCodecCtx->height);
+      if (!mFrameRGB) {
+        Fabric::EDK::throwException("Video Extension: Could not allocate RGB picture!");
+        return false;
+      }
+
+      /* open the output file, if needed */
+      if (!(fmt->flags & AVFMT_NOFILE)) {
+        if (url_fopen(&mFormatCtx->pb, filename.data(), URL_WRONLY) < 0) {
+          Fabric::EDK::throwException("Video Extension: Could not open output file!");
+          return false;
+        }
+      }
+      
+      /* write the stream header, if any */
+      av_write_header(mFormatCtx);
+    }
 
     return true;
   }
@@ -291,6 +428,35 @@ public:
     memcpy(&pixels[0],mFrameRGB->data[0],sizeof(KL::RGB) * getHeight() * getWidth());
     return true;
   }
+
+  bool writeFrame(KL::VariableArray<KL::RGB> &pixels)
+  {
+    if(mReadOnly)
+      return false;
+    
+    if(pixels.size() != getWidth() * getHeight())
+      return false;
+    memcpy(mFrameRGB->data[0],&pixels[0],sizeof(KL::RGB) * getHeight() * getWidth());
+
+    /* encode the image */
+    size_t out_size = avcodec_encode_video(mCodecCtx, mVideoBuffer, mVideoBufferSize, mFrameRGB);
+    /* if zero size, it means the image was buffered */
+    if (out_size > 0) {
+      AVPacket pkt;
+      av_init_packet(&pkt);
+
+      if (mCodecCtx->coded_frame->pts != AV_NOPTS_VALUE)
+        pkt.pts= av_rescale_q(mCodecCtx->coded_frame->pts, mCodecCtx->time_base, mVideoStream->time_base);
+      if(mCodecCtx->coded_frame->key_frame)
+        pkt.flags |= AV_PKT_FLAG_KEY;
+      pkt.stream_index= mVideoStream->index;
+      pkt.data = mVideoBuffer;
+      pkt.size = out_size;
+
+      /* write the compressed frame in the media file */
+      av_interleaved_write_frame(mFormatCtx, &pkt);
+    }
+  }
   
 };
 bool videoStream::sRegistered = false;
@@ -371,6 +537,34 @@ FABRIC_EXT_EXPORT void FabricVIDEOSeekTime(
   handle.pointer->seekTime(time);
 }
 
+FABRIC_EXT_EXPORT void FabricVIDEOCreateFromFileHandle(
+  KL::String file,
+  KL::Size width,
+  KL::Size height,
+  videoHandle &handle
+)
+{
+  if(handle.pointer == NULL)
+  {
+    KL::FileHandleWrapper wrapper(file);
+
+    // init the stream
+    handle.pointer = new videoStream(&handle);
+    if(!handle.pointer){
+      printf("=======> VIDEO-ERROR: Could not create videoStream object.\n");
+      handle.pointer = NULL;
+      return;
+    }
+    KL::String filePath = wrapper.getPath();
+    if(!handle.pointer->init(filePath,false,width,height))
+    {
+      delete(handle.pointer);
+      handle.pointer = NULL;
+      return;
+    }
+  }
+}
+
 FABRIC_EXT_EXPORT void FabricVIDEOGetAllPixels(
   videoHandle & handle,
   KL::VariableArray<KL::RGB> &pixels
@@ -379,6 +573,16 @@ FABRIC_EXT_EXPORT void FabricVIDEOGetAllPixels(
   if(handle.pointer == NULL)
     return;
   handle.pointer->getAllPixels(pixels);
+}
+
+FABRIC_EXT_EXPORT void FabricVIDEOWriteAllPixels(
+  videoHandle & handle,
+  KL::VariableArray<KL::RGB> &pixels
+)
+{
+  if(handle.pointer == NULL)
+    return;
+  handle.pointer->writeFrame(pixels);
 }
 
 FABRIC_EXT_EXPORT void FabricVIDEOFreeHandle(
