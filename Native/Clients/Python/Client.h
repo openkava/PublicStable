6dee1a21ba85a51e8864f2badea5d42b90b9c55f
Andrew MacPherson <andrew.macp@gmail.com>
2012-01-25 14:58:59 -0500

    Have notification callback into Python working now, need to fill in the contents of the notification.

--- Native/Clients/Python/Client.h
+++ Native/Clients/Python/Client.h
@@ -19,16 +19,20 @@ namespace Fabric
       static RC::Handle<Client> Create( RC::Handle<DG::Context> const &context );
       static RC::Handle<Client> Create();
       
-      virtual void notify( Util::SimpleString const &jsonEncodedNotifications ) const {};
+      virtual void notify( Util::SimpleString const &jsonEncodedNotifications ) const;
       void notifyInitialState() const
       {
         DG::Client::notifyInitialState();
       }
 
+      void setJSONNotifyCallback( void (*callback)(const char *) );
+
     protected:
    
       Client( RC::Handle<DG::Context> const &context );
       //~Client();
+
+      void (*m_notifyCallback)(const char *);
     };
   };
 };
