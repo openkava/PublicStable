a111ffeb0aa80e35e7777b07baf134b84bb9948d
Peter Zion <peter.zion@gmail.com>
2012-01-25 17:15:25 -0800

    Fix for handling of non-exportable types members

--- Native/Test/Fabric/rt-exportable-types.js
+++ Native/Test/Fabric/rt-exportable-types.js
@@ -1,12 +1,13 @@
 FC = require("Fabric").createClient();
 
 var node = FC.DG.createNode("foo");
+node.addMember("bar", "ValueProducer<Integer>");
 try {
-  node.addMember("bar", "Data");
+  console.log(node.getData("bar"));
   FC.flush();
 }
 catch (e) {
-  console.log("node.addMember: " + e);
+  console.log("node.getData('bar'): " + e);
 }
 
 FC.close();
