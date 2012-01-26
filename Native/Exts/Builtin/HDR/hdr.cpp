f4899c38de9a97fc05a49d64df08df9b0595c392
Helge Mathee <helge.mathee@fabric-engine.com>
2012-01-26 09:47:58 +0100

    EXR Extension: Support FileHandle

--- Native/Exts/Builtin/HDR/hdr.cpp
+++ Native/Exts/Builtin/HDR/hdr.cpp
@@ -227,3 +227,35 @@ FABRIC_EXT_EXPORT void FabricHDRDecode(
   
   readPixelsRLE( buffer, imagePixels, imageWidth, imageHeight );
 }
+
+FABRIC_EXT_EXPORT void FabricHDROpenFileHandle(
+  KL::String handle,
+  KL::Size &imageWidth,
+  KL::Size &imageHeight,
+  KL::VariableArray<KL::Color> &imagePixels
+  )
+{
+  KL::FileHandleWrapper wrapper(handle);
+  FILE * fp= fopen(wrapper.getPath().data(),"rb");
+  if(!fp)
+  {
+    Fabric::EDK::throwException("PNG Extension: File does not exist!");
+    return;
+  }
+  
+  fseek(fp, 0L, SEEK_END);
+  KL::Size size = ftell(fp);
+  fseek(fp, 0L, SEEK_SET);
+
+  KL::Data data = malloc(size);
+  if(fread(data,1,size,fp) != size)
+  {
+    fclose(fp);
+    Fabric::EDK::throwException("PNG Extension: Could not read file contents!");
+    return;
+  }
+  fclose(fp);
+  
+  FabricHDRDecode(data,size,imageWidth,imageHeight,imagePixels);
+  free(data);
+}
