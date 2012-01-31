545439210c16072b5757a358427334492226a85b
Andrew MacPherson <andrew.macp@gmail.com>
2012-01-31 17:47:25 -0500

    Porting more unit tests, fixing more bugs.

---
+++ Native/Test/Python/event-handler-binding-name.py
@@ -0,0 +1,31 @@
+import fabric
+F = fabric.createClient()
+
+o = F.DG.createOperator("o")
+o.setEntryFunctionName("foo")
+o.setSourceCode("\
+operator foo( io String string )\n\
+{\n\
+  report string;\n\
+}\n\
+")
+
+binding = F.DG.createBinding()
+binding.setOperator( o )
+binding.setParameterLayout( [ "peh.string" ] )
+
+ceh = F.DG.createEventHandler("ceh")
+ceh.preDescendBindings.append(binding)
+
+peh = F.DG.createEventHandler("peh")
+peh.setScopeName("peh")
+peh.addMember( "string", "String" )
+peh.setData( "string", "bar" )
+peh.appendChildEventHandler( ceh )
+
+e = F.DG.createEvent("e")
+e.appendEventHandler( peh )
+
+e.fire()
+
+F.close()
