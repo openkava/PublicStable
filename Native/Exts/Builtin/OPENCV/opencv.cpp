f8737efdf45eb80ebe41571b56de4f7c122033f8
Helge Mathee <helge.mathee@fabric-engine.com>
2012-02-03 11:17:47 +0100

    libface skeleton

---
+++ Native/Exts/Builtin/OPENCV/opencv.cpp
@@ -0,0 +1,16 @@
+/*
+ *  Copyright 2010-2011 Fabric Technologies Inc. All rights reserved.
+ */
+ 
+#include <Fabric/EDK/EDK.h>
+
+using namespace Fabric::EDK;
+IMPLEMENT_FABRIC_EDK_ENTRIES
+
+#include <opencv/cv.h>
+
+FABRIC_EXT_EXPORT void FabricOPENCVTest(
+)
+{
+  return;
+}
