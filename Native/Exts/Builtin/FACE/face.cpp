708589472f61a7a8420020427e8959193f56685e
Helge Mathee <helge.mathee@fabric-engine.com>
2012-02-03 11:17:47 +0100

    libface skeleton

---
+++ Native/Exts/Builtin/FACE/face.cpp
@@ -0,0 +1,16 @@
+/*
+ *  Copyright 2010-2011 Fabric Technologies Inc. All rights reserved.
+ */
+ 
+#include <Fabric/EDK/EDK.h>
+
+using namespace Fabric::EDK;
+IMPLEMENT_FABRIC_EDK_ENTRIES
+
+#include <LibFace.h>
+
+FABRIC_EXT_EXPORT void FabricFACETest(
+)
+{
+  return;
+}
