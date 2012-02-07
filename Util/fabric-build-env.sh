5c4d103fa90c5a3849611370d378596aee261033
Jerome Couture-Gagnon <jerome.couture-gagnon@fabric-engine.com>
2012-02-07 13:54:41 -0500

    Make "fabric-build-env.sh" work for Windows

--- Util/fabric-build-env.sh
+++ Util/fabric-build-env.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+windows_to_unix_path()
+{
+	UNIX_PATH_RESULT=/$(echo $1 | sed -e 's/\\/\//g' | sed 's#\:##g')
+}
+
 if [ -z "$FABRIC_CORE_PATH" ]; then
   cat >&2 <<EOF
 The FABRIC_CORE_PATH environment variable must be set
@@ -8,11 +13,20 @@ EOF
 else
   FABRIC_NPAPI_LINK=0
   FABRIC_BUILD_OS=$(uname -s)
-  if [ "$FABRIC_BUILD_OS" = "Darwin" ]; then
-    FABRIC_BUILD_ARCH=universal
-  else
-    FABRIC_BUILD_ARCH=$(uname -m)
-  fi
+  if [[ "$FABRIC_BUILD_OS" == *W32* ]]
+	then
+	  FABRIC_BUILD_OS="Windows"
+    FABRIC_BUILD_ARCH="x86"
+		FABRIC_CORE_PATH_WINDOWS_BACKUP=$FABRIC_CORE_PATH
+		windows_to_unix_path $FABRIC_CORE_PATH
+    FABRIC_CORE_PATH=$UNIX_PATH_RESULT
+	else
+	  if [ "$FABRIC_BUILD_OS" = "Darwin" ]; then
+		  FABRIC_BUILD_ARCH=universal
+		else
+			FABRIC_BUILD_ARCH=$(uname -m)
+		fi
+	fi
   FABRIC_BUILD_TYPE=
 
   OPTIND=0
@@ -51,54 +65,82 @@ EOF
       Linux)
         FABRIC_LIBRARY_DST_DIR="$HOME/.fabric"
         ;;
+      Windows)
+				FABRIC_CORE_PATH=$FABRIC_CORE_PATH_WINDOWS_BACKUP
+				windows_to_unix_path $APPDATA
+        FABRIC_LIBRARY_DST_DIR="$UNIX_PATH_RESULT/Fabric"
+        ;;
     esac
     echo "Creating directory $FABRIC_LIBRARY_DST_DIR"
     mkdir -p "$FABRIC_LIBRARY_DST_DIR"
 
-    case "$FABRIC_BUILD_OS" in
-      Darwin)
-        FABRIC_EXTS_SRC="$FABRIC_DIST_PATH/FabricEngine/Library/Fabric/Exts"
-        ;;
-      Linux)
-        FABRIC_EXTS_SRC="$FABRIC_DIST_PATH/Exts"
-        ;;
-    esac
+	  if [ "$FABRIC_BUILD_OS" = "Darwin" ]; then
+      FABRIC_EXTS_SRC="$FABRIC_DIST_PATH/FabricEngine/Library/Fabric/Exts"
+	  elif [ "$FABRIC_BUILD_OS" = "Windows" ]; then
+      FABRIC_EXTS_SRC="$FABRIC_DIST_PATH/FabricEngine/Exts"
+		else
+      FABRIC_EXTS_SRC="$FABRIC_DIST_PATH/Exts"
+		fi
+
     FABRIC_EXTS_DST="$FABRIC_LIBRARY_DST_DIR/Exts"
     echo "Setting up $FABRIC_EXTS_DST -> $FABRIC_EXTS_SRC"
     ln -snf "$FABRIC_EXTS_SRC" "$FABRIC_EXTS_DST"
 
-    FABRIC_NODE_MODULE_SRC="$FABRIC_DIST_PATH/FabricEngine/node_modules/Fabric"
-    FABRIC_NODE_MODULE_DST="$HOME/node_modules/Fabric"
-    echo "Linking $FABRIC_NODE_MODULE_DST -> $FABRIC_NODE_MODULE_SRC"
-    ln -snf "$FABRIC_NODE_MODULE_SRC" "$FABRIC_NODE_MODULE_DST"
+    if [ "$FABRIC_BUILD_OS" != "Windows" ]; then
+			FABRIC_NODE_MODULE_SRC="$FABRIC_DIST_PATH/FabricEngine/node_modules/Fabric"
+			FABRIC_NODE_MODULE_DST="$HOME/node_modules/Fabric"
+			echo "Linking $FABRIC_NODE_MODULE_DST -> $FABRIC_NODE_MODULE_SRC"
+			ln -snf "$FABRIC_NODE_MODULE_SRC" "$FABRIC_NODE_MODULE_DST"
 
-    FABRIC_PYTHON_MODULE_SRC="$FABRIC_DIST_PATH/FabricEngine/python_modules/fabric"
-    FABRIC_PYTHON_MODULE_DIR="$HOME/python_modules"
-    FABRIC_PYTHON_MODULE_DST="$FABRIC_PYTHON_MODULE_DIR/fabric"
-    echo "Linking $FABRIC_PYTHON_MODULE_DST -> $FABRIC_PYTHON_MODULE_SRC"
-    mkdir -p "$FABRIC_PYTHON_MODULE_DIR"
-    ln -snf "$FABRIC_PYTHON_MODULE_SRC" "$FABRIC_PYTHON_MODULE_DST"
+      FABRIC_PYTHON_MODULE_SRC="$FABRIC_DIST_PATH/FabricEngine/python_modules/fabric"
+      FABRIC_PYTHON_MODULE_DIR="$HOME/python_modules"
+      FABRIC_PYTHON_MODULE_DST="$FABRIC_PYTHON_MODULE_DIR/fabric"
+      echo "Linking $FABRIC_PYTHON_MODULE_DST -> $FABRIC_PYTHON_MODULE_SRC"
+      mkdir -p "$FABRIC_PYTHON_MODULE_DIR"
+      ln -snf "$FABRIC_PYTHON_MODULE_SRC" "$FABRIC_PYTHON_MODULE_DST"
+    fi
 
     case "$FABRIC_BUILD_OS" in
       Darwin)
         FABRIC_NPAPI_SRC="$FABRIC_DIST_PATH/FabricEngine/Library/Internet Plug-Ins/Fabric.$FABRIC_BUILD_ARCH.plugin"
-        FABRIC_NPAPI_DST_DIR="$HOME/Library/Internet Plug-Ins"
-        FABRIC_NPAPI_DST="$FABRIC_NPAPI_DST_DIR/Fabric.$FABRIC_BUILD_ARCH.plugin"
-        ;;
-      Linux)
-        FABRIC_NPAPI_SRC="$FABRIC_DIST_PATH/NPAPI/libFabricPlugin.so"
-        FABRIC_NPAPI_DST_DIR="$HOME/.mozilla/plugins"
-        FABRIC_NPAPI_DST="$FABRIC_NPAPI_DST_DIR/libFabricPlugin.so"
-        ;;
-    esac
-    if [ "$FABRIC_NPAPI_LINK" -eq 1 ]; then
-      echo "Creating directory $FABRIC_NPAPI_DST_DIR"
-      mkdir -p "$FABRIC_NPAPI_DST_DIR"
-      echo "Linking $FABRIC_NPAPI_DST -> $FABRIC_NPAPI_SRC"
-      ln -snf "$FABRIC_NPAPI_SRC" "$FABRIC_NPAPI_DST"
-    else
-      echo "Removing $FABRIC_NPAPI_DST"
-      rm -f "$FABRIC_NPAPI_DST"
-    fi
+				FABRIC_NPAPI_DST_DIR="$HOME/Library/Internet Plug-Ins"
+				FABRIC_NPAPI_DST="$FABRIC_NPAPI_DST_DIR/Fabric.$FABRIC_BUILD_ARCH.plugin"
+				;;
+			Linux)
+				FABRIC_NPAPI_SRC="$FABRIC_DIST_PATH/NPAPI/libFabricPlugin.so"
+				FABRIC_NPAPI_DST_DIR="$HOME/.mozilla/plugins"
+				FABRIC_NPAPI_DST="$FABRIC_NPAPI_DST_DIR/libFabricPlugin.so"
+				;;
+			Windows)
+		 		FABRIC_NPAPI_SRC="$FABRIC_DIST_PATH/FabricEngine"
+		esac
+		if [ "$FABRIC_NPAPI_LINK" -eq 1 ]; then
+
+		  if [ "$FABRIC_BUILD_OS" = "Windows" ]; then
+			  if [ -f $FABRIC_NPAPI_SRC/npFabricPlugin.dll ]; then
+					echo "Regristering NPAPI plugin"
+					pushd $FABRIC_NPAPI_SRC
+					regsvr32 //s npFabricPlugin.dll
+					popd
+				fi
+			else
+				echo "Creating directory $FABRIC_NPAPI_DST_DIR"
+				mkdir -p "$FABRIC_NPAPI_DST_DIR"
+				echo "Linking $FABRIC_NPAPI_DST -> $FABRIC_NPAPI_SRC"
+				ln -snf "$FABRIC_NPAPI_SRC" "$FABRIC_NPAPI_DST"
+			fi
+		else
+		  if [ "$FABRIC_BUILD_OS" = "Windows" ]; then
+			  if [ -f $FABRIC_NPAPI_SRC/npFabricPlugin.dll ]; then
+					echo "Unregristering NPAPI plugin"
+					pushd $FABRIC_NPAPI_SRC
+					regsvr32 //s //u npFabricPlugin.dll
+					popd
+				fi
+			else
+  			echo "Removing $FABRIC_NPAPI_DST"
+	  		rm -f "$FABRIC_NPAPI_DST"
+			fi
+		fi
   fi
 fi
