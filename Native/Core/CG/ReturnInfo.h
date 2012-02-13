43f064a257a6f6f06c31cd0689924ac2967158e4
Peter Zion <peter.zion@gmail.com>
2012-02-12 22:13:26 -0500

    Refactoring

---
+++ Native/Core/CG/ReturnInfo.h
@@ -0,0 +1,98 @@
+/*
+ *  Copyright 2010-2011 Fabric Technologies Inc. All rights reserved.
+ */
+
+#ifndef _FABRIC_CG_RETURN_INFO_H
+#define _FABRIC_CG_RETURN_INFO_H
+
+#include <Fabric/Core/CG/ExprType.h>
+
+namespace Fabric
+{
+  namespace CG
+  {
+    class ReturnInfo
+    {
+    public:
+    
+      ReturnInfo( ExprType const &exprType, llvm::Value *returnLValue = NULL )
+        : m_exprType( exprType )
+        , m_returnsStaticDataPtr( false )
+        , m_returnLValue( returnLValue )
+      {
+      }
+
+      ReturnInfo( ExprType const &exprType, bool returnsStaticDataPtr = false )
+        : m_exprType( exprType )
+        , m_returnsStaticDataPtr( returnsStaticDataPtr )
+        , m_returnLValue( NULL )
+      {
+      }
+
+      ReturnInfo( ReturnInfo const &that )
+        : m_exprType( that.m_exprType )
+        , m_returnsStaticDataPtr( that.m_returnsStaticDataPtr )
+        , m_returnLValue( that.m_returnLValue )
+      {
+      }
+
+      ReturnInfo &operator =( ReturnInfo const &that )
+      {
+        m_exprType = that.m_exprType;
+        m_returnsStaticDataPtr = that.m_returnsStaticDataPtr;
+        m_returnLValue = that.m_returnLValue;
+        return *this;
+      }
+
+      operator bool() const
+      {
+        return m_exprType.isValid();
+      }
+      
+      bool operator !() const
+      {
+        return !m_exprType.isValid();
+      }
+
+      ExprType const &getExprType() const
+      {
+        return m_exprType;
+      }
+
+      RC::ConstHandle<Adapter> getAdapter() const
+      {
+        return m_exprType.getAdapter();
+      }
+
+      CG::Usage getUsage() const
+      {
+        return m_exprType.getUsage();
+      }
+
+      std::string desc() const
+      {
+        return m_exprType.desc();
+      }
+
+      bool usesReturnLValue() const
+      {
+        if ( m_exprType.isValid() && !m_returnsStaticDataPtr )
+          return m_exprType.getAdapter()->usesReturnLValue();
+        else return false;
+      }
+
+      llvm::Value *getReturnLValue( ) const
+      {
+        return( m_returnLValue );
+      }
+
+    private:
+
+      ExprType m_exprType;
+      bool m_returnsStaticDataPtr;
+      llvm::Value *m_returnLValue;
+    };
+  }
+}
+
+#endif //_FABRIC_CG_RETURN_INFO_H
