3f99ec58af16bfede3696d5ad8e90fd02105ca4e
Peter Zion <peter.zion@gmail.com>
2012-02-15 16:23:50 -0500

    Fix another broken unit test

--- Native/Test/Python/issue-73.py
+++ Native/Test/Python/issue-73.py
@@ -1,8 +1,12 @@
 # test that setting slice count to 'None' gives exception
 # and not a crash
-import fabric
+import fabric, sys
 F = fabric.createClient()
 node = F.DG.createNode("foo")
-node.resize(None)
+try:
+  node.resize(None)
+  F.flush()
+except Exception as e:
+  print "Caught exception:", e
 node.evaluate()
 F.close()
