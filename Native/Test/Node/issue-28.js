d9154665ade70af055b8b3307160febf297b272a
Andrew MacPherson <andrew.macp@gmail.com>
2012-02-15 15:16:06 -0500

    Unit test for issue 28 and clean up after filestream test.

---
+++ Native/Test/Node/issue-28.js
@@ -0,0 +1,33 @@
+F = require('Fabric').createClient();
+
+op = F.DG.createOperator("videoSeekTime");
+op.setSourceCode(
+'use FabricVIDEO;\n'+
+'operator videoSeekTime(\n'+
+'  io VideoHandle handle,\n'+
+'  io Scalar time,\n'+
+'  io RGB pixels[],\n'+
+'  io RGB cache[][Integer]\n'+
+'  ) {\n'+
+'  Integer frameIndex = floor(time * handle.fps + 0.5);\n'+
+'  if(cache.has(frameIndex)) {\n'+
+'    pixels = cache[frameIndex];\n'+
+'    return;\n'+
+'  }\n'+
+'\n'+
+'  FabricVIDEOSeekTime(handle, time);\n'+
+'  FabricVIDEOGetAllPixels(handle, pixels);\n'+
+'\n'+
+'  cache[frameIndex] = pixels;\n'+
+'}\n'
+);
+op.setEntryFunctionName("videoSeekTime");
+if (op.getErrors().length > 0) {
+  console.log(op.getErrors());
+  if (op.getDiagnostics().length > 0) {
+    console.log(op.getDiagnostics());
+  }
+}
+
+F.close();
+
