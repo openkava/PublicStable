696e87060c9a4e23c2f24de09ae268e03a2935b3
Andrew MacPherson <andrew.macp@gmail.com>
2012-02-16 14:34:26 -0500

    Fix final valgrind memory error, issue 128 + unit test.

---
+++ Native/Test/Python/issue-128.py
@@ -0,0 +1,25 @@
+import fabric
+fabricClient = fabric.createClient()
+
+# taken from the register-types test, just the small
+# segment that causes the memory leak
+class ComplexType():
+  def __init__( self ):
+    mystr = "foooooooo"
+    vec = []
+    for i in range(0, 4):
+      vec.append( mystr )
+    vec_vec = []
+    for i in range(0, 6):
+      vec_vec.append( vec )
+    self.member = vec_vec
+
+complexTypeDesc = {
+  'members': [ { 'member': 'String[][]' } ],
+  'constructor': ComplexType
+}
+
+fabricClient.RT.registerType('ComplexType', complexTypeDesc)
+fabricClient.flush()
+fabricClient.close()
+
