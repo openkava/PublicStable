0a82d10c4a6a0876ab41d7b1bf93569eb1139557
Philip Taylor <phil.taylor@fabric-engine.com>
2012-02-15 22:39:21 -0500

    * Fixed bump when animation clip is looped and the error is calculated.

--- SG/CharacterLocomotion.js
+++ SG/CharacterLocomotion.js
@@ -548,6 +548,7 @@ FABRIC.SceneGraph.registerNodeType('ScreenSpacePlayerCharacterController', {
   
 FABRIC.RT.TrackSetController = function( activeTrackSet ) {
   this.activeTrackSet = activeTrackSet != undefined ? activeTrackSet : 0;
+  this.activeTrackSetTime = 0;
   this.time = 0.0;
   this.currKeys = [];
   this.trackWeight = 0.0;
@@ -563,6 +564,7 @@ FABRIC.appendOnCreateContextCallback(function(context) {
   context.RegisteredTypesManager.registerType('TrackSetController', {
     members: {
       activeTrackSet: 'Integer',
+      activeTrackSetTime: 'Scalar',
       time: 'Scalar',
       currKeys: 'Integer[]',
       trackWeight: 'Scalar',
