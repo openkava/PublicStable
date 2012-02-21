40cd13bbadb3d5c14405de3f1be323c9cd4a5bd1
Andrew MacPherson <andrew.macp@gmail.com>
2012-02-21 14:31:57 -0500

    Fix copyright for Python files.

--- Native/Test/Python/klc-compilation.py
+++ Native/Test/Python/klc-compilation.py
@@ -1,3 +1,7 @@
+#
+#  Copyright 2010-2012 Fabric Technologies Inc. All rights reserved.
+#
+
 import fabric
 fabricClient = fabric.createClient()
 KLC = fabricClient.KLC.createCompilation("foo.kl", "operator foo() { bad }")
