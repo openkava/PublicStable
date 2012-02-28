cb29d8e9c4d900b75c881f54dc2505fb48c68d92
Phil Taylor <phil.taylor@fabric-engine.com>
2012-02-28 12:56:56 -0500

    * Increment the version number to '1.0.22-release'.

--- Core/FABRIC.js
+++ Core/FABRIC.js
@@ -4,8 +4,8 @@
 
 var FABRIC = (function() {
 
-  var requiredVersion = [1,0,21];
-  var requiredVersionSuffix = "-beta";
+  var requiredVersion = [1,0,22];
+  var requiredVersionSuffix = "-release";
   var fullRequiredVersionString = requiredVersion.join('.') + requiredVersionSuffix;
 
   // we keep an array of context ids,
