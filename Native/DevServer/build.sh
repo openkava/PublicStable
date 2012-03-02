7848d0152e16671312cd986585927c7a5bb4151e
Peter Zion <peter.zion@gmail.com>
2012-03-02 17:08:16 -0500

    Remove DevServer since it has never really been used for anything

--- Native/DevServer/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-arch -i386 py2applet-2.7 FabricDevServer.py
-cp -r /opt/local/lib/Resources/qt_menu.nib FabricDevServer.app/Contents/Resources/
-cp -r Fabric.icns FabricDevServer.app/Contents/Resources/PythonApplet.icns
