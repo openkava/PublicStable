1bb33556d6f01c65bb75c6ae6fa4bb88cfff01ee
Peter Zion <peter.zion@gmail.com>
2012-03-04 15:06:52 -0500

    Add --tag-name-filter to clean-public.sh

--- Util/clean-public.sh
+++ Util/clean-public.sh
@@ -3,7 +3,10 @@
 # Copyright 2010-2011 Fabric Technologies Inc. All rights reserved.
 #
 
-git filter-branch -f --prune-empty --index-filter 'git rm -r --cached --ignore-unmatch \
-  Native/Installer/Chrome/ChromeExtension.pem \
-  Native/Installer/Firefox/xpi-update.pem \
-  ' -- --all
+git filter-branch -f --prune-empty --index-filter '\
+  git rm -r --cached --ignore-unmatch \
+    Native/Installer/Chrome/ChromeExtension.pem \
+    Native/Installer/Firefox/xpi-update.pem \
+  ' \
+  --tag-name-filter cat \
+  -- --all
