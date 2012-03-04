0109733ba9d2bec69a95a1f07866885dd32e1025
Peter Zion <peter.zion@gmail.com>
2012-03-04 15:38:23 -0500

    Add more exclusions to Util/clean-public.sh

--- Util/clean-public.sh
+++ Util/clean-public.sh
@@ -7,6 +7,10 @@ git filter-branch -f --prune-empty --index-filter '\
   git rm -r --cached --ignore-unmatch \
     Native/Installer/Chrome/ChromeExtension.pem \
     Native/Installer/Firefox/xpi-update.pem \
+    Native/Clients/NPAPI/Windows/afxres.h \
+    Native/Clients/NPAPI/Windows/winres.h \
+    Native/ThirdParty/Private/uhura.pl \
+    Python \
   ' \
   --tag-name-filter cat \
   -- --all
