e0a27649965998d459713d4979d61552d1c81044
Peter Zion <peter.zion@gmail.com>
2012-03-13 11:15:28 -0400

    Fix running of unit tests

--- Native/Test/JSON/run.sh
+++ Native/Test/JSON/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 TEST_SUFFIX="json"
-TEST_CMD="../../build/$FABRIC_BUILD_OS/$FABRIC_BUILD_ARCH/$FABRIC_BUILD_TYPE/Fabric/Tools/ParseJSON/parseJSON"
+TEST_CMD="../../../build/Native/$FABRIC_BUILD_OS/$FABRIC_BUILD_ARCH/$FABRIC_BUILD_TYPE/Fabric/Tools/ParseJSON/parseJSON"
 
 . ../runtests.sh
 
